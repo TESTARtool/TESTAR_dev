15e53f2811f9bdea6b3c3a562d262b36dfe2c5e5
florendg <florendg@gmail.com>
2017-06-04 17:45:41 +0200

    Implemented storing a State object in the Graph database.

--- graphdb/src/edu/ou/testar/OrientDBRepository.java
+++ graphdb/src/edu/ou/testar/OrientDBRepository.java
@@ -3,9 +3,12 @@ package edu.ou.testar;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
-import com.tinkerpop.blueprints.impls.orient.OrientGraphNoTx;
 import org.fruit.alayer.State;
-import org.fruit.alayer.Tag;
+import org.fruit.alayer.Tags;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.NoSuchElementException;
 
 /**
  * Implementation of the interaction with the Graph database. This class is implemented as a Singleton.
@@ -13,31 +16,64 @@ import org.fruit.alayer.Tag;
  */
 public class OrientDBRepository {
 
-   private final OrientGraphFactory graphFactory;
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrientDBRepository.class);
+
+    private final OrientGraphFactory graphFactory;
 
 
-   public OrientDBRepository(final OrientGraphFactory factory) {
-      graphFactory =  factory;
-   }
+    public OrientDBRepository(final OrientGraphFactory factory) {
+        graphFactory = factory;
+    }
 
-   /**
-    * Store State in Graph database.
-    *
-    * @param state State of the SUT for this step.
-    */
-   public void addState(final State state) {
-      OrientGraph graph = graphFactory.getTx();
-      try {
-         Vertex v = graph.addVertex("class:State");
-         for (Tag t : state.tags()) {
-            v.setProperty(t.name(), t);
-         }
-         graph.commit();
-      } finally {
-         graph.shutdown();
-      }
-   }
+    /**
+     * Store State in Graph database.
+     * @param state State of the SUT for this step.
+     */
+    public void addState(final State state) {
+        LOGGER.info("Store state {}",state.get(Tags.ConcreteID));
+        OrientGraph graph = graphFactory.getTx();
+        try {
+            Vertex v = getStateVertex(state.get(Tags.ConcreteID), graph);
+            if (v == null) {
+               createStateVertex(state,graph);
+            } else {
+                int i = v.getProperty("visited");
+                v.setProperty("visited", i + 1);
+            }
+            graph.commit();
+        } finally {
+            graph.shutdown();
+        }
+        LOGGER.info("state {} stored",state.get(Tags.ConcreteID));
+    }
 
+    /**
+     * Create new State Vertex
+     * @param state State which needs to be stored as a vertex
+     * @param graph Handle to the graph database.
+     */
+    private void createStateVertex(final State state, final OrientGraph graph){
+        Vertex vertex = graph.addVertex("class:State");
+        state.tags().forEach(t -> vertex.setProperty(t.name(), state.get(t)));
+        vertex.setProperty("visited", 1);
+    }
 
+    /**
+     * Lookup state vertex in the database
+     * @param concrteID unique identification of the state
+     * @param graph handle to the graph database
+     * @return the vertex of for the State object or null if the state is not found.
+     */
+    private Vertex getStateVertex(String concrteID, OrientGraph graph) {
+        try {
+            Iterable<Vertex> vertices = graph.getVertices("State." + Tags.ConcreteID, concrteID);
+            Vertex vertex = vertices.iterator().next();
+            LOGGER.debug("Vertex {} found",vertex.getId());
+            return vertex;
+        } catch (IllegalArgumentException | NoSuchElementException ex) {
+            LOGGER.debug("There is no vertex inserted yet for the given State ConcreteID {}", concrteID);
+            return null;
+        }
+    }
 
 }
--- graphdb/test/edu/ou/testar/OrientDBRepositoryTest.java
+++ graphdb/test/edu/ou/testar/OrientDBRepositoryTest.java
@@ -1,6 +1,5 @@
 package edu.ou.testar;
 
-import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
 import org.fruit.alayer.StdState;
@@ -9,46 +8,60 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
+ * Unit test to validate the storage of State objects
+ * A State will be stored ones. Everytime the same state is stored a flag visited is updated.
  * Created by florendegier on 03/06/2017.
  */
 public class OrientDBRepositoryTest {
 
-   OrientGraphFactory factory;
+    private OrientGraphFactory factory;
 
-   @Before
-   public void setup() {
-      factory = new OrientGraphFactory("memory:demo");
-      factory.setMaxRetries(10);
-      factory.setupPool(1, 2);
-      factory.setRequireTransaction(false);
-   }
+    @Before
+    public void setup() {
+        factory = new OrientGraphFactory("memory:demo");
+        factory.setMaxRetries(10);
+        factory.setupPool(1, 2);
+        factory.setRequireTransaction(false);
+    }
 
-   @After
-   public void cleanup() {
-      factory.close();
-   }
+    @After
+    public void cleanup() {
+        factory.close();
+    }
 
-   @Test
-   public void addState() {
-      OrientDBRepository sut = new OrientDBRepository(factory);
+    @Test
+    public void testAddState() {
+        OrientDBRepository sut = new OrientDBRepository(factory);
 
-      StdState state  = new StdState();
-      state.set(Tags.ConcreteID,"0xCAFE");
+        StdState state = new StdState();
+        state.set(Tags.ConcreteID, "0xCAFE");
 
-      sut.addState(state);
+        sut.addState(state);
 
-      OrientGraph graph = factory.getTx();
+        OrientGraph graph = factory.getTx();
+        long data = graph.getRawGraph().countClass("State");
+        assertEquals("The database shall contain one element", 1, data);
+        graph.shutdown();
 
-      long data =graph.getRawGraph().countClass("State");
+        sut.addState(state);
 
-      assertEquals("The database shall contain one element",1,data);
+        graph = factory.getTx();
+        data = graph.getRawGraph().countClass("State");
+        assertEquals("The database shall contain one element", 1, data);
 
-      graph.shutdown();
+        state.set(Tags.ConcreteID, "0xDEAD");
 
+        sut.addState(state);
 
-   }
+        graph = factory.getTx();
+        data = graph.getRawGraph().countClass("State");
+        assertEquals("The database shall contain two elements", 2, data);
+
+        graph.shutdown();
+
+    }
 
 }
