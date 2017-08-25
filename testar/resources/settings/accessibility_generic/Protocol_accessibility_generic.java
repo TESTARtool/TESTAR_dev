4a04674d0fac743011c3e33c48843ebf054215da
Davy Kager <mail@davykager.nl>
2017-08-25 13:48:52 +0200

    Add WCAG2ICT protocol settings.

---
+++ testar/resources/settings/accessibility_generic/Protocol_accessibility_generic.java
@@ -0,0 +1,25 @@
+/**********************************************************************************************
+ *                                                                                            *
+ * COPYRIGHT (2017):                                                                          *
+ * Universitat Politecnica de Valencia                                                        *
+ * Camino de Vera, s/n                                                                        *
+ * 46022 Valencia, Spain                                                                      *
+ * www.upv.es                                                                                 *
+ *                                                                                            * 
+ * D I S C L A I M E R:                                                                       *
+ * This software has been developed by the Universitat Politecnica de Valencia (UPV)          *
+ * in the context of the STaQ (Software Testing and Quality) research group: staq.dsic.upv.es *
+ * This software is distributed FREE of charge under the TESTAR license, as an open           *
+ * source project under the BSD3 license (http://opensource.org/licenses/BSD-3-Clause)        *                                                                                        * 
+ *                                                                                            *
+ **********************************************************************************************/
+
+import nl.ou.testar.a11y.protocols.WCAG2ICTProtocol;
+
+/**
+ * A generic accessibility protocol based on WCAG2ICT
+ * @author Davy Kager
+ */
+public class Protocol_accessibility_generic extends WCAG2ICTProtocol {
+
+}
