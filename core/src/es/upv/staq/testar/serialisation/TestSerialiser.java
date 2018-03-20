f64779338ae79573d719d34ca8cf000d7ca82981
testar <testar@10.0.2.15>
2018-03-20 11:45:23 +0100

    Solving merge problems from Egit

--- core/src/es/upv/staq/testar/serialisation/TestSerialiser.java
+++ core/src/es/upv/staq/testar/serialisation/TestSerialiser.java
@@ -1,18 +1,31 @@
-/**********************************************************************************************
- *                                                                                            *
- * COPYRIGHT (2016):                                                                          *
- * Universitat Politecnica de Valencia                                                        *
- * Camino de Vera, s/n                                                                        *
- * 46022 Valencia, Spain                                                                      *
- * www.upv.es                                                                                 *
- *                                                                                            * 
- * D I S C L A I M E R:                                                                       *
- * This software has been developed by the Universitat Politecnica de Valencia (UPV)          *
- * in the context of the STaQ (Software Testing and Quality) research group: staq.dsic.upv.es *
- * This software is distributed FREE of charge under the TESTAR license, as an open           *
- * source project under the BSD3 license (http://opensource.org/licenses/BSD-3-Clause)        *                                                                                        * 
- *                                                                                            *
- **********************************************************************************************/
+/***************************************************************************************************
+*
+* Copyright (c) 2016, 2017 Universitat Politecnica de Valencia - www.upv.es
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+* this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holder nor the names of its
+* contributors may be used to endorse or promote products derived from
+* this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************************************/
 
 
 package es.upv.staq.testar.serialisation;
