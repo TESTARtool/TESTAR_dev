6fec5f620c8c86795782b691ab935998f09ab591
florendg <florendg@gmail.com>
2018-04-15 15:08:34 +0200

    restore default tools path

--- windows/native_src/clean_w10.bat
+++ windows/native_src/clean_w10.bat
@@ -1,3 +1,3 @@
-CALL "C:\ToolsMicrosoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=amd64
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=amd64
 
 nmake -f Makefile_w10 clean
