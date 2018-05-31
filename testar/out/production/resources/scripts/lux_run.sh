2b83867b41a6dd10c9e99a596e29775625955614
pekkakaho <33083138+pekkakaho@users.noreply.github.com>
2018-05-31 15:29:24 +0300

    removing binaries
    removing binaries

--- testar/out/production/resources/scripts/lux_run.sh
+++
@@ -1,1 +0,0 @@
-java -ea -server -Xmx2g $* -jar testar.jar | tee console_out.txt
--- testar/out/production/resources/scripts/reportmetrics.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-@REM script by urueda 2017
-if "%1"=="" GOTO MISS
-GOTO GO
-:MISS
-echo Argument missing
-echo Use: reportmetrics metrics_folder
-:GO
-@FOR %%A IN (B C D E F G H I J K L M N) DO @metricstats.bat %1 %%%%A
