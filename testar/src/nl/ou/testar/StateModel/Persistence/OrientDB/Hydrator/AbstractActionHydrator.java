8ec128deb57c7a10a6ecc512478ce62e6052f6ec
Adrian Mulders <admulders@gmail.com>
2018-09-17 06:42:09 +0200

    finished persistence of state transitions

---
+++ testar/src/nl/ou/testar/StateModel/Persistence/OrientDB/Hydrator/AbstractActionHydrator.java
@@ -0,0 +1,4 @@
+package nl.ou.testar.StateModel.Persistence.OrientDB.Hydrator;
+
+public class AbstractActionHydrator {
+}
---
+++ testar/src/nl/ou/testar/StateModel/Util/HydrationHelper.java
@@ -0,0 +1,4 @@
+package nl.ou.testar.StateModel.Util;
+
+public class HydrationHelper {
+}
