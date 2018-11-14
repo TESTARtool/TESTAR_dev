82d3c64e99bc0c7568ef386b4f021fbf234ef3a1
pekkakaho <33083138+pekkakaho@users.noreply.github.com>
2018-11-14 17:47:27 +0200

    testar version
    Should we put just 2.0 or for example 2.0_#139 (where 139 is the number of pull request merged in the master)

--- testar/src/org/fruit/monkey/SettingsDialog.java
+++ testar/src/org/fruit/monkey/SettingsDialog.java
@@ -68,7 +68,7 @@ import static org.fruit.monkey.dialog.ToolTipTexts.*;
 public class SettingsDialog extends JFrame implements Observer {
   private static final long serialVersionUID = 5156320008281200950L;
 
-  static final String TESTAR_VERSION = "v1.3";
+  static final String TESTAR_VERSION = "2.0_#139";
 
   private String settingsFile;
   private Settings settings;
