6ce959c4040608ac7dbaf9824d24b4aea25e6c91
Fernando Pastor <32359126+ferpasri@users.noreply.github.com>
2018-11-22 10:13:11 +0100

    Basic settings for Qualitate
    Basic settings for Qualitate

--- testar/resources/settings/web_generic/Protocol_web_generic.java
+++ testar/resources/settings/web_generic/Protocol_web_generic.java
@@ -1,62 +1,88 @@
 /***************************************************************************************************
-*
-* Copyright (c) 2013, 2014, 2015, 2016, 2017 Universitat Politecnica de Valencia - www.upv.es
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*
-* 1. Redistributions of source code must retain the above copyright notice,
-* this list of conditions and the following disclaimer.
-* 2. Redistributions in binary form must reproduce the above copyright
-* notice, this list of conditions and the following disclaimer in the
-* documentation and/or other materials provided with the distribution.
-* 3. Neither the name of the copyright holder nor the names of its
-* contributors may be used to endorse or promote products derived from
-* this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
-*******************************************************************************************************/
+ *
+ * Copyright (c) 2013, 2014, 2015, 2016, 2017, 2018 Universitat Politecnica de Valencia - www.upv.es
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************************************/
 
 
 import java.io.File;
 import java.util.Set;
 
 import org.fruit.Drag;
-import org.fruit.alayer.*;
+import org.fruit.Util;
+import org.fruit.alayer.AbsolutePosition;
+import org.fruit.alayer.Action;
+import org.fruit.alayer.Canvas;
+import org.fruit.alayer.Point;
 import org.fruit.alayer.exceptions.ActionBuildException;
+import org.fruit.alayer.Role;
+import org.fruit.alayer.Roles;
+import org.fruit.alayer.SUT;
+import org.fruit.alayer.Shape;
+import org.fruit.alayer.State;
 import org.fruit.alayer.exceptions.StateBuildException;
 import org.fruit.alayer.exceptions.SystemStartException;
+import org.fruit.alayer.Verdict;
+import org.fruit.alayer.Widget;
 import org.fruit.alayer.actions.AnnotatingActionCompiler;
+import org.fruit.alayer.actions.CompoundAction;
+import org.fruit.alayer.actions.KeyDown;
 import org.fruit.alayer.actions.StdActionCompiler;
-import es.upv.staq.testar.protocols.ClickFilterLayerProtocol; 
+import org.fruit.alayer.actions.Type;
+import org.fruit.alayer.devices.AWTKeyboard;
+import org.fruit.alayer.devices.KBKeys;
+import org.fruit.alayer.devices.Keyboard;
+
+import es.upv.staq.testar.protocols.ClickFilterLayerProtocol;
+import es.upv.staq.testar.serialisation.LogSerialiser;
+import nl.ou.testar.RandomActionSelector;
+import es.upv.staq.testar.ActionStatus;
 import es.upv.staq.testar.NativeLinker;
 import org.fruit.monkey.ConfigTags;
 import org.fruit.monkey.Settings;
-
+import org.fruit.alayer.Tags;
 import static org.fruit.alayer.Tags.Blocked;
 import static org.fruit.alayer.Tags.Enabled;
 
 public class Protocol_web_generic extends ClickFilterLayerProtocol {
-	
-	// This protocol expects Mozilla Firefox or Microsoft Internet Explorer on Windows10
 
-	static Role webText; // browser dependent
+	// This protocol expects Mozilla Firefox or Microsoft Internet Explorer on Windows10
+	static final int BROWSER_IEXPLORER = 1;
+	static final int BROWSER_FIREFOX = 2;
+	static final int BROWSER_CHROME = 3;
+
+	// browser dependent variables
+	static int browser;
+	static Role webText;
 	static double browser_toolbar_filter;
 
 	//Attributes for adding slide actions
 	static double scrollArrowSize = 36; // sliding arrows (iexplorer)
 	static double scrollThick = 16; // scroll thickness (iexplorer)
-	
+
 	/** 
 	 * Called once during the life time of TESTAR
 	 * This method can be used to perform initial setup work
@@ -66,23 +92,49 @@ public class Protocol_web_generic extends ClickFilterLayerProtocol {
 		super.initialize(settings);
 		initBrowser();
 	}
-	
+
 	// check browser
 	private void initBrowser(){
-		webText = NativeLinker.getNativeRole("UIAEdit"); // just init with some value
+		//Get the SUT connector string from the settings
 		String sutPath = settings().get(ConfigTags.SUTConnectorValue);
-		if (sutPath.contains("iexplore.exe"))
+
+		//Check which browser we use to connect to the web application
+		//and set the browser dependent variables accordingly
+		if (sutPath.contains("iexplore.exe")) {
+			browser = BROWSER_IEXPLORER;
 			webText = NativeLinker.getNativeRole("UIAEdit");
-		else if (sutPath.contains("firefox"))
+		}
+		else if (sutPath.contains("firefox")) {
+			browser = BROWSER_FIREFOX;
 			webText = NativeLinker.getNativeRole("UIAText");
+		}
+		else if (sutPath.contains("chrome") || sutPath.contains("chromium")) {
+			browser = BROWSER_CHROME;
+			webText = NativeLinker.getNativeRole("UIAEdit");
+		}
+		else {
+			browser = 0;
+			webText = NativeLinker.getNativeRole("UIAEdit");
+		}
+	}
+
+	/**
+	 * This method is invoked each time TESTAR starts to generate a new sequence
+	 */
+	protected void beginSequence(SUT system, State state){
+
+		super.beginSequence(system, state);
+
 	}
 
 	/**
 	 * This method is called when TESTAR starts the System Under Test (SUT). The method should
-	 * take care of
+	 * take care of 
 	 *   1) starting the SUT (you can use TESTAR's settings obtainable from <code>settings()</code> to find
 	 *      out what executable to run)
-	 *   2) waiting until the system is fully loaded and ready to be tested (with large systems, you might have to wait several
+	 *   2) bringing the system into a specific start state which is identical on each start (e.g. one has to delete or restore
+	 *      the SUT's configuratio files etc.)
+	 *   3) waiting until the system is fully loaded and ready to be tested (with large systems, you might have to wait several
 	 *      seconds until they have finished loading)
 	 * @return  a started SUT, ready to be tested.
 	 */
@@ -90,19 +142,36 @@ public class Protocol_web_generic extends ClickFilterLayerProtocol {
 
 		SUT sut = super.startSystem();
 
+		//wait a bit to give the UI some time and get focus
+		Util.pause(3);
+
+		//Enter password on .add(new Type(""),0.1)
+		new CompoundAction.Builder()   
+		.add(new Type(""),0.1)
+		.build()
+		.run(sut,null, 0.1);   
+
+		//wait a bit to give the UI some time and get focus
+		Util.pause(1);
+
+		//login is performed by ENTER
+		new CompoundAction.Builder()
+		.add(new KeyDown(KBKeys.VK_ENTER),0.5)
+		.build()
+		.run(sut, null, 0.1); 
+
+		Util.pause(1);
+
+		//Domain selection is performed by ENTER 
+		new CompoundAction.Builder()
+		.add(new KeyDown(KBKeys.VK_ENTER),0.5)
+		.build()
+		.run(sut, null, 0.1);
+
+		//Wait x seconds
+		Util.pause(3);
+
 		return sut;
-
-	}
-
-	/**
-	 * This method is invoked each time the TESTAR starts the SUT to generate a new sequence.
-	 * This can be used for example for bypassing a login screen by filling the username and password
-	 * or bringing the system into a specific start state which is identical on each start (e.g. one has to delete or restore
-	 * the SUT's configuration files etc.)
-	 */
-	protected void beginSequence(SUT system, State state){
-		
-		super.beginSequence(system, state);
 
 	}
 
@@ -115,17 +184,17 @@ public class Protocol_web_generic extends ClickFilterLayerProtocol {
 	 * @return  the current state of the SUT with attached oracle.
 	 */
 	protected State getState(SUT system) throws StateBuildException{
-		
+
 		State state = super.getState(system);
 
-        for(Widget w : state){
-            Role role = w.get(Tags.Role, Roles.Widget);
-            if(Role.isOneOf(role, new Role[]{NativeLinker.getNativeRole("UIAToolBar")}))
-            	browser_toolbar_filter = w.get(Tags.Shape,null).y() + w.get(Tags.Shape,null).height();
-        }
-		
+		for(Widget w : state){
+			Role role = w.get(Tags.Role, Roles.Widget);
+			if(Role.isOneOf(role, new Role[]{NativeLinker.getNativeRole("UIAToolBar")}))
+				browser_toolbar_filter = w.get(Tags.Shape,null).y() + w.get(Tags.Shape,null).height();
+		}
+
 		return state;
-		
+
 	}
 
 	/**
@@ -134,18 +203,17 @@ public class Protocol_web_generic extends ClickFilterLayerProtocol {
 	 * @return oracle verdict, which determines whether the state is erroneous and why.
 	 */
 	protected Verdict getVerdict(State state){
-		
+		// The super methods implements the implicit online state oracles for:
+		// system crashes
+		// non-responsiveness
+		// suspicious titles
 		Verdict verdict = super.getVerdict(state);
-		// system crashes, non-responsiveness and suspicious titles automatically detected!
-		
-		//-----------------------------------------------------------------------------
-		// MORE SOPHISTICATED ORACLES CAN BE PROGRAMMED HERE (the sky is the limit ;-)
-        //-----------------------------------------------------------------------------
 
-		// ... YOU MAY WANT TO CHECK YOUR CUSTOM ORACLES HERE ...
-		
+		//--------------------------------------------------------
+		// MORE SOPHISTICATED STATE ORACLES CAN BE PROGRAMMED HERE
+		//--------------------------------------------------------
+
 		return verdict;
-
 	}
 
 	/**
@@ -159,41 +227,83 @@ public class Protocol_web_generic extends ClickFilterLayerProtocol {
 	 * @return  a set of actions
 	 */
 	protected Set<Action> deriveActions(SUT system, State state) throws ActionBuildException{
-		
+
 		Set<Action> actions = super.deriveActions(system,state);
 		// unwanted processes, force SUT to foreground, ... actions automatically derived!
 
 		// create an action compiler, which helps us create actions, such as clicks, drag&drop, typing ...
 		StdActionCompiler ac = new AnnotatingActionCompiler();
-		
-		//----------------------
-		// BUILD CUSTOM ACTIONS
-		//----------------------
 
+		//PopUp of IE SUT
+		if(!state.get(Tags.Foreground, true) && system.get(Tags.SystemActivator, null) != null){
+			LOGGER.warn("Ventana de alerta mostrada al usuario");
+			Keyboard kb = AWTKeyboard.build();
+
+			new CompoundAction.Builder()
+			.add(new KeyDown(KBKeys.VK_ENTER),0.5)
+			.build()
+			.run(system,null, 0.5);
+
+			kb.release(KBKeys.VK_ENTER);
+		}		
 		// iterate through all widgets
 		for(Widget w : getTopWidgets(state)){
-			if(w.get(Enabled, true) && !w.get(Blocked, false)){ // only consider enabled and non-blocked widgets
-				if (!blackListed(w)){  // do not build actions for tabu widgets
 
-					// left clicks
-					if(whiteListed(w) || isClickable(w))
+			//Check current browser tab, to close possible undesired tabs
+			if(w.get(Tags.Title,"").toString().contains("Address and search")) {
+				if(!w.get(Tags.ValuePattern,"").toString().contains("vims-alm")) {
+
+					Keyboard kb = AWTKeyboard.build();
+					CompoundAction cAction = new CompoundAction(new KeyDown(KBKeys.VK_CONTROL),new KeyDown(KBKeys.VK_W));
+
+					executeAction(system, state, cAction);
+
+					kb.release(KBKeys.VK_CONTROL);
+					kb.release(KBKeys.VK_W);
+				}
+			}
+			// Only consider enabled and non-blocked widgets
+			if(w.get(Enabled, true) && !w.get(Blocked, false)){
+
+				// Do not build actions for widgets on the blacklist
+				// The blackListed widgets are those that have been filtered during the SPY mode with the
+				//CAPS_LOCK + SHIFT + Click clickfilter functionality.
+				if (!blackListed(w)){
+
+					//For widgets that are:
+					// - clickable
+					// and
+					// - unFiltered by any of the regular expressions in the Filter-tab, or
+					// - whitelisted using the clickfilter functionality in SPY mode (CAPS_LOCK + SHIFT + CNTR + Click)
+					// We want to create actions that consist of left clicking on them
+					if(isClickable(w) && (isUnfiltered(w) || whiteListed(w))) {
+						//Store the widget in the Graphdatabase
+						storeWidget(state.get(Tags.ConcreteID), w);
+						//Create a left click action with the Action Compiler, and add it to the set of derived actions
 						actions.add(ac.leftClickAt(w));
+					}
 
-					// type into text boxes
-					if(whiteListed(w) || isTypeable(w))
+					//For widgets that are:
+					// - typeable
+					// and
+					// - unFiltered by any of the regular expressions in the Filter-tab, or
+					// - whitelisted using the clickfilter functionality in SPY mode (CAPS_LOCK + SHIFT + CNTR + Click)
+					// We want to create actions that consist of typing into them
+					if(isTypeable(w) && (isUnfiltered(w) || whiteListed(w))) {
+						//Store the widget in the Graphdatabase
+						storeWidget(state.get(Tags.ConcreteID), w);
+						//Create a type action with the Action Compiler, and add it to the set of derived actions
 						actions.add(ac.clickTypeInto(w, this.getRandomText(w)));
-
-					// slides
+					}
+					//Add sliding actions (like scroll, drag and drop) to the derived actions
+					//method defined below.
 					addSlidingActions(actions,ac,scrollArrowSize,scrollThick,w,state);
-
 				}
 			}
 		}
-
+		//return the set of derived actions
 		return actions;
-
 	}
-
 	/**
 	 * Adds sliding actions (like scroll, drag and drop) to the given Set of Actions
 	 * @param actions
@@ -214,13 +324,12 @@ public class Protocol_web_generic extends ClickFilterLayerProtocol {
 				actions.add(ac.slideFromTo(
 						new AbsolutePosition(Point.from(drag.getFromX(),drag.getFromY())),
 						new AbsolutePosition(Point.from(drag.getToX(),drag.getToY()))
-				));
+						));
 
 			}
 		}
 	}
 
-	@Override
 	protected boolean isClickable(Widget w){
 		if (isAtBrowserCanvas(w))
 			return super.isClickable(w);
@@ -228,15 +337,14 @@ public class Protocol_web_generic extends ClickFilterLayerProtocol {
 			return false;		
 	} 
 
-	@Override
 	protected boolean isTypeable(Widget w){
 		if (!isAtBrowserCanvas(w))
 			return false;	
-		
+
 		Role role = w.get(Tags.Role, null);
 		if (role != null && Role.isOneOf(role, webText))
 			return isUnfiltered(w);
-		
+
 		return false;
 	}
 
@@ -247,17 +355,22 @@ public class Protocol_web_generic extends ClickFilterLayerProtocol {
 		else
 			return false;		
 	}
-	
+
 	/**
-	 * Select one of the possible actions (e.g. at random)
+	 * Select one of the available actions (e.g. at random)
 	 * @param state the SUT's current state
-	 * @param actions the set of available actions as computed by <code>buildActionsSet()</code>
+	 * @param actions the set of derived actions
 	 * @return  the selected action (non-null!)
 	 */
 	protected Action selectAction(State state, Set<Action> actions){
-		
-		return super.selectAction(state, actions);
-		
+		//Call the preSelectAction method from the AbstractProtocol so that, if necessary,
+		//unwanted processes are killed and SUT is put into foreground.
+		Action a = preSelectAction(state, actions);
+		if (a!= null) {
+			return a;
+		} else
+			//if no preSelected actions are needed, then implement your own strategy
+			return RandomActionSelector.selectAction(actions);
 	}
 
 	/**
@@ -268,43 +381,66 @@ public class Protocol_web_generic extends ClickFilterLayerProtocol {
 	 * @return whether or not the execution succeeded
 	 */
 	protected boolean executeAction(SUT system, State state, Action action){
+		//Save URL of web browser in the logs (Generate)
+		/*for(Widget w: state) {
+			if(w.get(Tags.Title,"").toString().contains("Address and search")) {
+				LogSerialiser.log(String.format("URL of state: %s\n",w.get(Tags.ValuePattern,"").toString()));
+				LOGGER.info(String.format("URL of state: %s",w.get(Tags.ValuePattern,"").toString()));
+			}
+		}*/
 		
 		return super.executeAction(system, state, action);
-		
 	}
-	
+	protected void waitUserActionLoop(Canvas cv, SUT system, State state, ActionStatus actionStatus){
+		super.waitUserActionLoop(cv, system, state, actionStatus);
+		//Save URL of web browser in the logs (Generate Manual)
+		/*for(Widget w: state) {
+			if(w.get(Tags.Title,"").toString().contains("Address and search")) {
+				LogSerialiser.log(String.format("URL of state: %s\n",w.get(Tags.ValuePattern,"").toString()));
+				LOGGER.info(String.format("URL of state: %s",w.get(Tags.ValuePattern,"").toString()));
+			}
+		}*/
+	}
+
 	/**
 	 * TESTAR uses this method to determine when to stop the generation of actions for the
-	 * current sequence. You could stop the sequence's generation after a given amount of executed
-	 * actions or after a specific time etc.
+	 * current sequence. You can stop deriving more actions after:
+	 * - a specified amount of executed actions, which is specified through the SequenceLength setting, or
+	 * - after a specific time, that is set in the MaxTime setting
 	 * @return  if <code>true</code> continue generation, else stop
 	 */
 	protected boolean moreActions(State state) {
-		
 		return super.moreActions(state);
-		
 	}
 
 
+	/**
+	 * TESTAR uses this method to determine when to stop the entire test sequence
+	 * You could stop the test after:
+	 * - a specified amount of sequences, which is specified through the Sequences setting, or
+	 * - after a specific time, that is set in the MaxTime setting
+	 * @return  if <code>true</code> continue test, else stop
+	 */
+	protected boolean moreSequences() {
+		return super.moreSequences();
+	}
+
+	/**
+	 * Here you can put graceful shutdown sequence for your SUT
+	 * @param system
+	 */
+	@Override
+	protected void stopSystem(SUT system) {
+		super.stopSystem(system);
+	}
+	
 	/** 
 	 * This method is invoked each time after TESTAR finished the generation of a sequence.
 	 */
 	protected void finishSequence(){
-		
+
 		super.finishSequence();
-		
+
 	}
 
-
-	/**
-	 * TESTAR uses this method to determine when to stop the entire test.
-	 * You could stop the test after a given amount of generated sequences or
-	 * after a specific time etc.
-	 * @return  if <code>true</code> continue test, else stop	 */
-	protected boolean moreSequences() {
-		
-		return super.moreSequences();
-		
-	}
-		
 }
