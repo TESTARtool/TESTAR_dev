79e5b4c8caf4972f3b3b85d8321b1eb46bb03258
LENOVOCONNY\CONNY <Conny.Hageluken@kpnmail.nl>
2018-11-22 17:05:03 +0100

    stop tracking files

--- core/src/org/fruit/alayer/actions/UrlActionCompiler.java
+++ core/src/org/fruit/alayer/actions/UrlActionCompiler.java
@@ -15,44 +15,48 @@ import org.fruit.alayer.devices.KBKeys;
 import org.fruit.alayer.devices.MouseButtons;
 
 public class UrlActionCompiler extends StdActionCompiler {
-	private Abstractor abstractor;
-	private final Action LMouseDown = new MouseDown(MouseButtons.BUTTON1);
-	private final Action LMouseUp = new MouseUp(MouseButtons.BUTTON1);
-		
-	public UrlActionCompiler() {	
-		this(new StdAbstractor()); 
-		}
+  private Abstractor abstractor;
+  private final Action LMouseDown = new MouseDown(MouseButtons.BUTTON1);
+  private final Action LMouseUp = new MouseUp(MouseButtons.BUTTON1);
+    
+  public UrlActionCompiler() {  
+    this(new StdAbstractor()); 
+  }
 
-	public UrlActionCompiler(Abstractor abstractor) {
-		this.abstractor = abstractor;
-	}
+  public UrlActionCompiler(Abstractor abstractor) {
+    this.abstractor = abstractor;
+  }
 
-	public Action clickTypeUrl(final Position position, final String text, int currentUrlLength) {
-		Assert.notNull(position, text);
-		// Go to the beginning and click there
-		Builder builder = new CompoundAction.Builder();
-		builder.add(new MouseMove(position), 2).add(LMouseDown, 1).add(LMouseUp, 1);
-		builder.add(new KeyDown(KBKeys.VK_HOME), 1).add(new KeyUp(KBKeys.VK_HOME), 1);
-		// Delete the current text
-		for (int i = 0; i < currentUrlLength + 10; i++) {
-			builder.add(new KeyDown(KBKeys.VK_DELETE), 1).add(new KeyUp(KBKeys.VK_DELETE), 1);
-		}
-		builder.add(new Type(text), 1);
-		builder.add(new KeyDown(KBKeys.VK_ENTER), 1);
-		return builder.build();
-	}
+  public Action clickTypeUrl(final Position position, final String text, int currentUrlLength) {
+    Assert.notNull(position, text);
+    // Go to the beginning and click there
+    Builder builder = new CompoundAction.Builder();
+    builder.add(new MouseMove(position), 2).add(LMouseDown, 1).add(LMouseUp, 1);
+    builder.add(new MouseMove(position), 2).add(LMouseDown, 1).add(LMouseUp, 1);
+    builder.add(new MouseMove(position), 2).add(LMouseDown, 1).add(LMouseUp, 1);
+    builder.add(new KeyDown(KBKeys.VK_HOME), 1).add(new KeyUp(KBKeys.VK_HOME), 1);
+    // Delete the current text
+    for (int i = 0; i < currentUrlLength + 10; i++) {
+      builder.add(new KeyDown(KBKeys.VK_DELETE), 1).add(new KeyUp(KBKeys.VK_DELETE), 1);
+    }
+    builder.add(new Type(text), 1);
+    builder.add(new KeyDown(KBKeys.VK_ENTER), 1);
+    return builder.build();
+  }
 
-	public Action clickTypeUrl(Widget widget, String text) {
-		return clickTypeUrl(widget, 0.5, 0.5, text);
-	}
-	
-	public Action clickTypeUrl(Widget widget, double relX, double relY, String text) {
-		String value = widget.get(Tags.ValuePattern);
-		int currentUrlLength = value.length();
-		Finder wf = abstractor.apply(widget);
-		Action ret = clickTypeUrl(new WidgetPosition(wf, Tags.Shape, relX, relY, true), text, currentUrlLength);
-		ret.set(Tags.Targets, Util.newArrayList(wf));
-		ret.set(Tags.TargetID, widget.get(Tags.ConcreteID));
-		return ret;
-	}
+  public Action clickTypeUrl(Widget widget, String text) {
+    return clickTypeUrl(widget, 0.5, 0.5, text);
+  }
+  
+  public Action clickTypeUrl(Widget widget, double relX, double relY, String text) {
+    String value = widget.get(Tags.ValuePattern);
+    int currentUrlLength = value.length();
+    Finder wf = abstractor.apply(widget);
+    Position p = new WidgetPosition(wf, Tags.Shape, relX, relY, true);
+    Action ret =
+        clickTypeUrl(new WidgetPosition(wf, Tags.Shape, relX, relY, true), text, currentUrlLength);
+    ret.set(Tags.Targets, Util.newArrayList(wf));
+    ret.set(Tags.TargetID, widget.get(Tags.ConcreteID));
+    return ret;
+  }
 }
