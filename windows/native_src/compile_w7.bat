be00f44c2915dbbc759bc09972753cc8709b74f7
pekkakaho <33083138+pekkakaho@users.noreply.github.com>
2018-12-03 19:17:52 +0200

    fixing Win7 build scripts
    fixing Win7 build scripts

--- windows/native_src/compile_w7.bat
+++ windows/native_src/compile_w7.bat
@@ -1,6 +1,6 @@
 :: set MSVCDIR=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin
 :: call "%MSVCDIR%\vcvars32.bat"
 
-CALL "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+CALL "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /release /x86
 
-nmake -f Makefile_w7
+ nmake -f Makefile_w7
