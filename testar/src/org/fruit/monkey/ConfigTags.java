0012a000521c1f2cb9c889366346ee6d4b38e33c
Guy Theuws <guy_theuws@hotmail.com>
2019-03-13 08:00:34 +0100

    Format file

--- testar/src/org/fruit/monkey/ConfigTags.java
+++ testar/src/org/fruit/monkey/ConfigTags.java
@@ -1,31 +1,32 @@
-/***************************************************************************************************
-*
-* Copyright (c) 2013, 2014, 2015, 2016, 2017, 2018 Universitat Politecnica de Valencia - www.upv.es
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*
-* 1. Redistributions of source code must retain the above copyright notice,
-* this list of conditions and the following disclaimer.
-* 2. Redistributions in binary form must reproduce the above copyright
-* notice, this list of conditions and the following disclaimer in the
-* documentation and/or other materials provided with the distribution.
-* 3. Neither the name of the copyright holder nor the names of its
-* contributors may be used to endorse or promote products derived from
-* this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
-*******************************************************************************************************/
+/**
+ * Copyright (c) 2013, 2014, 2015, 2016, 2017, 2018 Universitat Politecnica de Valencia - www.upv.es
+ * <p>
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * <p>
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Sebastian Bauersfeld
+ */
 
 
 /**
@@ -102,26 +103,26 @@ public final class ConfigTags {
     public static final Tag<String> GraphDBUser = Tag.from("GraphDBUser", String.class);
     public static final Tag<String> GraphDBPassword = Tag.from("GraphDBPassword", String.class);
 
-  // state model config tags
-  public static final Tag<Boolean> StateModelEnabled = Tag.from("StateModelEnabled", Boolean.class);
-  public static final Tag<String> DataStore = Tag.from("DataStore", String.class);
-  public static final Tag<String> DataStoreType = Tag.from("DataStoreType", String.class);
-  public static final Tag<String> DataStoreServer = Tag.from("DataStoreServer", String.class);
-  public static final Tag<String> DataStoreDB = Tag.from("DataStoreDB", String.class);
-  public static final Tag<String> DataStoreUser = Tag.from("DataStoreUser", String.class);
-  public static final Tag<String> DataStorePassword = Tag.from("DataStorePassword", String.class);
-  public static final Tag<String> DataStoreMode = Tag.from("DataStoreMode", String.class);
-  public static final Tag<Boolean> ResetDataStore = Tag.from("ResetDataStore", Boolean.class);
-  @SuppressWarnings("unchecked")
-  public static final Tag<List<String>> ConcreteStateAttributes = Tag.from("ConcreteStateAttributes", (Class<List<String>>) (Class<?>) List.class);
-  @SuppressWarnings("unchecked")
-  public static final Tag<List<String>> AbstractStateAttributes = Tag.from("AbstractStateAttributes", (Class<List<String>>) (Class<?>) List.class);
+    // state model config tags
+    public static final Tag<Boolean> StateModelEnabled = Tag.from("StateModelEnabled", Boolean.class);
+    public static final Tag<String> DataStore = Tag.from("DataStore", String.class);
+    public static final Tag<String> DataStoreType = Tag.from("DataStoreType", String.class);
+    public static final Tag<String> DataStoreServer = Tag.from("DataStoreServer", String.class);
+    public static final Tag<String> DataStoreDB = Tag.from("DataStoreDB", String.class);
+    public static final Tag<String> DataStoreUser = Tag.from("DataStoreUser", String.class);
+    public static final Tag<String> DataStorePassword = Tag.from("DataStorePassword", String.class);
+    public static final Tag<String> DataStoreMode = Tag.from("DataStoreMode", String.class);
+    public static final Tag<Boolean> ResetDataStore = Tag.from("ResetDataStore", Boolean.class);
+    @SuppressWarnings("unchecked")
+    public static final Tag<List<String>> ConcreteStateAttributes = Tag.from("ConcreteStateAttributes", (Class<List<String>>) (Class<?>) List.class);
+    @SuppressWarnings("unchecked")
+    public static final Tag<List<String>> AbstractStateAttributes = Tag.from("AbstractStateAttributes", (Class<List<String>>) (Class<?>) List.class);
 
-  public static final Tag<Boolean> AlwaysCompile = Tag.from("AlwaysCompile", Boolean.class);
+    public static final Tag<Boolean> AlwaysCompile = Tag.from("AlwaysCompile", Boolean.class);
 
-  public static final Tag<Boolean> ProcessListenerEnabled = Tag.from("ProcessListenerEnabled", Boolean.class);
-  public static final Tag<String> SuspiciousProcessOutput = Tag.from("SuspiciousProcessOutput", String.class);
-  public static final Tag<String> ProcessLogs = Tag.from("ProcessLogs", String.class);
+    public static final Tag<Boolean> ProcessListenerEnabled = Tag.from("ProcessListenerEnabled", Boolean.class);
+    public static final Tag<String> SuspiciousProcessOutput = Tag.from("SuspiciousProcessOutput", String.class);
+    public static final Tag<String> ProcessLogs = Tag.from("ProcessLogs", String.class);
     public static final Tag<String> StrategyFile = Tag.from("StrategyFile", String.class);
     public static final Tag<String> InputFileText = Tag.from("InputFileText", String.class);
 }
