250bfec966c6d25b53d75b90bdb1c319e25fd20a
CaSe <35034352+carlosengers@users.noreply.github.com>
2019-05-05 22:17:31 +0200

    start dash server (attempt)

--- testar/src/nl/ou/testar/tl/TemporalPanel.java
+++ testar/src/nl/ou/testar/tl/TemporalPanel.java
@@ -34,29 +34,33 @@ import org.fruit.monkey.Settings;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.io.*;
 
 /**
  * Panel with settings for the state model inference module.
  */
 public class TemporalPanel extends JPanel {
 
-	private static final long serialVersionUID = -2815422165938359999L;
+    private static final long serialVersionUID = -2815422165938359999L;
 
 
-    private JLabel label20 = new JLabel("LTL property");
+    private JLabel label00 = new JLabel("Temporal property");
     private JTextField temporalProperty = new JTextField("G(a->Fb)");
     private JButton modelcheckButton = new JButton("Check property");
-    private JTextArea checkResult = new JTextArea("...");
-    private JButton temporalButton = new JButton("Temporal Analyzer");
+    private JLabel label02 = new JLabel("Check result");
+    private JTextArea logCheckResult = new JTextArea("...");
+    private JPanel logPanel;
+    private JScrollPane resultsScrollPane;
+    private JButton temporalButton = new JButton("<html>Start Temporal<br>WebAnalyzer</html>");
 
 
-
-    private TemporalPanel(){
+    private TemporalPanel() {
         super();
     }
 
     /**
      * Create and Initialize StateModelPanel.
+     *
      * @return StateModelPanel.
      */
     public static TemporalPanel createStateModelPanel() {
@@ -73,23 +77,39 @@ public class TemporalPanel extends JPanel {
 
         // add the components to the panel
         setLayout(null);
-        label20.setBounds(10,14,150,27);
-        add(label20);
-        temporalProperty.setBounds(160,14,125,27);
+        label00.setBounds(10, 14, 350, 27);
+        add(label00);
+        temporalProperty.setBounds(160, 14, 125, 27);
         add(temporalProperty);
 
 
         modelcheckButton.setBounds(10, 42, 120, 27);
         modelcheckButton.addActionListener(this::performTemporalCheck);
-        modelcheckButton.setToolTipText("Select the 'databases' folder in your orientdb installation. Make sure the OrientDB server is not running.");
+        modelcheckButton.setToolTipText("Check whether the property holds on the current State Model.");
         add(modelcheckButton);
+        label02.setBounds(10, 80, 350, 27);
+        add(label02);
+        logPanel = new JPanel();
 
-        checkResult.setBounds(10,80,150,227);
-        add(checkResult);
+        logCheckResult.setBounds(160, 80, 350, 277);
+        logCheckResult.setAutoscrolls(true);
+        logCheckResult.setLineWrap(true);
+        logCheckResult.setWrapStyleWord(true);
+        //resultsScrollPane = new JScrollPane(logCheckResult,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        //resultsScrollPane.setSize(340,210);
+        //resultsScrollPane.setBounds(160,80,250,227);
+        //logPanel.add(resultsScrollPane);
+        //logPanel.setBounds(160,80,350,227);
+        //logPanel.setSize(340,227);
+        //add(logPanel);
 
-        temporalButton.setBounds(10, 316, 120, 27);
-        temporalButton.addActionListener(this::performTemporalCheck);
-        temporalButton.setToolTipText("Select the 'databases' folder in your orientdb installation. Make sure the OrientDB server is not running.");
+        add(logCheckResult);
+
+        temporalButton.setBounds(10, 316, 120, 54);
+        temporalButton.setSize(temporalButton.getWidth(), 45);
+
+        temporalButton.addActionListener(this::startTemporalWebAnalyzer);
+        temporalButton.setToolTipText("Visual Analytics of the temporal property on the current State Model.\n Gto: http\\localhost:8050");
         add(temporalButton);
 
 
@@ -100,6 +120,7 @@ public class TemporalPanel extends JPanel {
 
     /**
      * Populate GraphDBFields from Settings structure.
+     *
      * @param settings The settings to load.
      */
     public void populateFrom(final Settings settings) {
@@ -108,6 +129,7 @@ public class TemporalPanel extends JPanel {
 
     /**
      * Retrieve information from the GraphDB GUI.
+     *
      * @param settings reference to the object where the settings will be stored.
      */
     public void extractInformation(final Settings settings) {
@@ -115,12 +137,92 @@ public class TemporalPanel extends JPanel {
     }
 
 
-
-
-
     private void performTemporalCheck(ActionEvent evt) {
-       // code goes here
+        // code goes here
+        Process theProcess = null;
+        BufferedReader inStream = null;
+        String cli = "ubuntu1804 run /mnt/c/Users/c/OneDrive/OU/AF/Ubuntu/spotparse '" + temporalProperty.getText() + "'";
+        String response;
+        logCheckResult.setText("invoking : ");
+        logCheckResult.append("\n");
+        logCheckResult.setText(cli);
+        logCheckResult.append("\n");
+        // call the external program
+        try {
+            theProcess = Runtime.getRuntime().exec(cli);
+        } catch (IOException e) {
+            System.err.println("Error on exec() method");
+            logCheckResult.append("Error on exec() method");
+            logCheckResult.append("\n");
+            e.printStackTrace();
+        }
+
+        // read from the called program's standard output stream
+        try {
+            inStream = new BufferedReader(new InputStreamReader
+                    (theProcess.getInputStream()));
+            while ((response = inStream.readLine()) != null) {
+                System.out.println("response: " + response);
+                logCheckResult.append(response);
+                logCheckResult.append("\n");
+            }
+
+            logCheckResult.append("------------Action completed---------------" + "\n");
+        } catch (IOException e) {
+            System.err.println("Error on inStream.readLine()");
+            logCheckResult.append("Error on inStream.readLine()");
+            logCheckResult.append("\n");
+            e.printStackTrace();
         }
     }
 
+    private void startTemporalWebAnalyzer(ActionEvent evt) {
+        // code goes here
 
+
+        Process theProcess = null;
+        BufferedReader inStream = null;
+        String cli_part1 = "C:\\Users\\c\\git\\Testar_viz\\run.py";
+        String cli = "C:\\Users\\c\\git\\TESTAR_dev\\testar\\src\\nl\\ou\\testar\\tl\\Start_Python.bat "+cli_part1;
+
+        String response;
+        logCheckResult.setText("invoking : ");
+        logCheckResult.append("\n");
+        logCheckResult.setText(cli);
+        logCheckResult.append("\n");
+        // call the external program
+        try
+        {
+            theProcess = Runtime.getRuntime().exec(cli);
+        }
+        catch(IOException e)
+        {
+            System.err.println("Error on exec() method");
+            logCheckResult.append("Error on exec() method");
+            logCheckResult.append("\n");
+            e.printStackTrace();
+        }
+
+        // read from the called program's standard output stream
+        try
+        {
+            inStream = new BufferedReader(new InputStreamReader
+                    (theProcess.getInputStream()));
+            while ((response = inStream.readLine()) != null) {
+                System.out.println("response: " + response);
+                logCheckResult.append(response);
+                logCheckResult.append("\n");
+            }
+
+            logCheckResult.append("------------Action completed---------------"+"\n");
+        }
+        catch(IOException e)
+        {
+            System.err.println("Error on inStream.readLine()");
+            logCheckResult.append("Error on inStream.readLine()");
+            logCheckResult.append("\n");
+            e.printStackTrace();
+        }
+    }
+
+}
