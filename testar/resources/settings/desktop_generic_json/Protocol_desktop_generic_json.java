4631b27d92ac2614a5ec704925e36d3461735f5f
pekkakaho <33083138+pekkakaho@users.noreply.github.com>
2019-05-14 12:03:21 +0300

    refactored the JSON code
    refactored the JSON code, creating a new Java class for easier use from other protocols

--- testar/resources/settings/desktop_generic_json/Protocol_desktop_generic_json.java
+++ testar/resources/settings/desktop_generic_json/Protocol_desktop_generic_json.java
@@ -106,46 +106,7 @@ public class Protocol_desktop_generic_json extends ClickFilterLayerProtocol {
 	@Override
 	protected State getState(SUT system) throws StateBuildException{
 		State state = super.getState(system);
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		Set<WidgetJsonObject> widgetJsonObjects = new HashSet<WidgetJsonObject>();
-		for(Widget widget:state){
-			boolean enabled = widget.get(Tags.Enabled, null);
-			String role = widget.get(Tags.Role, null).toString();
-			boolean blocked = widget.get(Tags.Blocked, null);
-			Rect rect = (Rect) widget.get(Tags.Shape, null);
-			Set<Vertice> vertices = new HashSet<Vertice>();
-			vertices.add(new Vertice(rect.x(), rect.y()));
-			vertices.add(new Vertice(rect.x()+rect.width(), rect.y()));
-			vertices.add(new Vertice(rect.x(), rect.y()+rect.height()));
-			vertices.add(new Vertice(rect.x()+rect.width(), rect.y()+rect.height()));
-			BoundingPoly boundingPoly = new BoundingPoly(vertices);
-			String className= widget.get(UIATags.UIAClassName, "");
-			String title= widget.get(Tags.Title, "");
-			String desc= widget.get(Tags.Desc, "");
-			String name= widget.get(UIATags.UIAName, "");
-			String toolTipText= widget.get(Tags.ToolTipText, "");
-			String valuePattern= widget.get(Tags.ValuePattern, "");
-			WidgetJsonObject widgetJsonObject = new WidgetJsonObject(enabled, role, blocked, boundingPoly, className, title, desc, name, toolTipText, valuePattern);
-			widgetJsonObjects.add(widgetJsonObject);
-//			for(Tag tag:widget.tags()){
-//				System.out.println("Tag:"+tag.toString()+"="+widget.get(tag));
-//			}
-		}
-		System.out.println("Widget size="+widgetJsonObjects.size());
-		String screenshotPath = state.get(Tags.ScreenshotPath);
-		System.out.println("ScreenshotPath="+screenshotPath);
-		ScreenshotWidgetJsonObject screenshotWidgetJsonObject = new ScreenshotWidgetJsonObject(widgetJsonObjects, screenshotPath);
-		System.out.println("JSON:"+ gson.toJson(screenshotWidgetJsonObject));
-		String filePath = screenshotPath.substring(0, screenshotPath.lastIndexOf("."))+".json";
-		System.out.println("FilePath="+filePath);
-		try{
-			FileWriter fileWriter = new FileWriter(filePath);
-			gson.toJson(screenshotWidgetJsonObject, fileWriter);
-			fileWriter.flush(); //flush data to file   <---
-			fileWriter.close(); //close write          <---
-		}catch(Exception e){
-			System.out.println("ERROR: Writing JSON into file failed!");
-		}
+		JsonUtils.createWidgetInfoJsonFile(state);
 		return state;
 	}
 
