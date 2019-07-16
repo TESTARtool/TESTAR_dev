b08dd82c9e5d99850cfe0afef29bced29a3c18a8
CaSe <35034352+carlosengers@users.noreply.github.com>
2019-07-16 20:10:41 +0200

    sandbox  repair compile and clean .bat

--- windows/native_src/compile_w10.bat
+++ windows/native_src/compile_w10.bat
@@ -1,4 +1,7 @@
-#CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64
-#css 20190131
+#one of the 2 wil work, the other will fail without sideeffects
+#css 20190716 for @home
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64
+
+#css 20190716 for @work
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" -arch=x64
 nmake -f Makefile_w10
