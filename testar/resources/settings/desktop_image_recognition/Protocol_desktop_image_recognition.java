d8fc05ece960adff52071e9486cf4275925b2d2f
Fernando Pastor <32359126+ferpasri@users.noreply.github.com>
2019-07-26 14:51:17 +0200

    Add imageRecognition protocol
    Add imageRecognition protocol

---
+++ testar/resources/settings/desktop_image_recognition/Protocol_desktop_image_recognition.java
@@ -0,0 +1,363 @@
+package desktop_image_recognition;
+/***************************************************************************************************
+ *
+ * Copyright (c) 2013, 2014, 2015, 2016, 2017, 2018, 2019 Universitat Politecnica de Valencia - www.upv.es
+ * Copyright (c) 2018, 2019 Open Universiteit - www.ou.nl
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************************************/
+
+
+import static org.fruit.alayer.Tags.Blocked;
+import static org.fruit.alayer.Tags.Enabled;
+
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.imageio.ImageIO;
+
+import org.fruit.Util;
+import org.fruit.alayer.*;
+import org.fruit.alayer.actions.AnnotatingActionCompiler;
+import org.fruit.alayer.actions.StdActionCompiler;
+import org.fruit.alayer.exceptions.*;
+import org.fruit.alayer.windows.UIATags;
+import org.fruit.monkey.Main;
+import org.fruit.monkey.Settings;
+import org.testar.OutputStructure;
+import org.testar.protocols.DesktopProtocol;
+
+import es.upv.staq.testar.serialisation.ScreenshotSerialiser;
+import net.sourceforge.tess4j.ITessAPI;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.TessAPI;
+import net.sourceforge.tess4j.TessAPI1;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.Word;
+import net.sourceforge.tess4j.util.LoadLibs;
+
+/**
+ * This protocol provides default TESTAR behaviour to test Windows desktop applications.
+ *
+ * It uses random action selection algorithm.
+ */
+public class Protocol_desktop_image_recognition extends DesktopProtocol {
+
+	private HashMap<Rect, String> imageWidgets = new HashMap<> ();
+	String pathImage = "";
+
+	/**
+	 * Called once during the life time of TESTAR
+	 * This method can be used to perform initial setup work
+	 * @param   settings  the current TESTAR settings as specified by the user.
+	 */
+	@Override
+	protected void initialize(Settings settings){
+		try {
+			File outputImages = new File(Main.outputDir + File.separator + "Tesseract");
+			outputImages.mkdir();
+
+			ScreenshotSerialiser.exit();
+			ScreenshotSerialiser.start(outputImages.getAbsolutePath(), "");
+
+		}catch(Exception e) {}
+
+		super.initialize(settings);
+	}
+
+	/**
+	 * This methods is called before each test sequence, before startSystem(),
+	 * allowing for example using external profiling software on the SUT
+	 *
+	 * HTML sequence report will be initialized in the super.preSequencePreparations() for each sequence
+	 */
+	@Override
+	protected void preSequencePreparations() {
+		super.preSequencePreparations();
+	}
+
+	/**
+	 * This method is called when TESTAR starts the System Under Test (SUT). The method should
+	 * take care of
+	 *   1) starting the SUT (you can use TESTAR's settings obtainable from <code>settings()</code> to find
+	 *      out what executable to run)
+	 *   2) waiting until the system is fully loaded and ready to be tested (with large systems, you might have to wait several
+	 *      seconds until they have finished loading)
+	 * @return  a started SUT, ready to be tested.
+	 */
+	@Override
+	protected SUT startSystem() throws SystemStartException{
+		return super.startSystem();
+	}
+
+	/**
+	 * This method is invoked each time the TESTAR starts the SUT to generate a new sequence.
+	 * This can be used for example for bypassing a login screen by filling the username and password
+	 * or bringing the system into a specific start state which is identical on each start (e.g. one has to delete or restore
+	 * the SUT's configuration files etc.)
+	 */
+	@Override
+	protected void beginSequence(SUT system, State state){
+		super.beginSequence(system, state);
+	}
+
+	/**
+	 * This method is called when the TESTAR requests the state of the SUT.
+	 * Here you can add additional information to the SUT's state or write your
+	 * own state fetching routine. The state should have attached an oracle
+	 * (TagName: <code>Tags.OracleVerdict</code>) which describes whether the
+	 * state is erroneous and if so why.
+	 *
+	 * super.getState(system) puts the state information also to the HTML sequence report
+	 *
+	 * @return  the current state of the SUT with attached oracle.
+	 */
+	@Override
+	protected State getState(SUT system) throws StateBuildException{
+
+		State state = super.getState(system);
+
+		imageWidgets.clear();
+
+		try {
+
+			if(pathImage.length() > 1) {
+				boolean deleteold = new File(pathImage).delete();
+
+				//System.out.println("Old screenshot deleted: " + deleteold + pathImage);
+			}
+
+			pathImage = ScreenshotSerialiser.saveStateshot(state.get(Tags.ConcreteID,"NoConcreteID"), protocolUtil.getStateshotBinary(state));
+
+			Util.pause(1);
+
+			Tesseract tess = new Tesseract();
+
+			File tessDataFolder = LoadLibs.extractTessResources("tessdata");
+			tess.setDatapath(tessDataFolder.getAbsolutePath());
+
+			BufferedImage image = ImageIO.read(new File(pathImage));
+
+			for(Word word : tess.getWords(image, ITessAPI.TessPageIteratorLevel.RIL_TEXTLINE)) {
+
+				Rectangle rectangle = word.getBoundingBox();
+
+				Rect rect = Rect.fromCoordinates(rectangle.getMinX(), rectangle.getMinY(),
+						rectangle.getMaxX(), rectangle.getMaxY());
+
+				//Testar recognizes Spy mode dots like dot text
+				if(word.getText().length() > 2) {
+					imageWidgets.put(rect, word.getText());
+
+					//System.out.println(word.getText());
+				}
+
+			}
+
+		}catch(Exception e) {}
+
+		return state;
+	}
+
+	/**
+	 * The getVerdict methods implements the online state oracles that
+	 * examine the SUT's current state and returns an oracle verdict.
+	 * @return oracle verdict, which determines whether the state is erroneous and why.
+	 */
+	@Override
+	protected Verdict getVerdict(State state){
+		// The super methods implements the implicit online state oracles for:
+		// system crashes
+		// non-responsiveness
+		// suspicious titles
+		Verdict verdict = super.getVerdict(state);
+
+		//--------------------------------------------------------
+		// MORE SOPHISTICATED STATE ORACLES CAN BE PROGRAMMED HERE
+		//--------------------------------------------------------
+
+		return verdict;
+	}
+
+	/**
+	 * This method is used by TESTAR to determine the set of currently available actions.
+	 * You can use the SUT's current state, analyze the widgets and their properties to create
+	 * a set of sensible actions, such as: "Click every Button which is enabled" etc.
+	 * The return value is supposed to be non-null. If the returned set is empty, TESTAR
+	 * will stop generation of the current action and continue with the next one.
+	 * @param system the SUT
+	 * @param state the SUT's current state
+	 * @return  a set of actions
+	 */
+	@Override
+	protected Set<Action> deriveActions(SUT system, State state) throws ActionBuildException{
+
+		Set<Action> actions = super.deriveActions(system,state);
+
+		// To derive actions (such as clicks, drag&drop, typing ...) we should first create an action compiler.
+		StdActionCompiler ac = new AnnotatingActionCompiler();
+
+		// To find all possible actions that TESTAR can click on we should iterate through all widgets of the state.
+		for(Widget w : state){
+
+			// Only consider enabled and non-blocked widgets
+
+			//if(w.get(Enabled, true) && !w.get(Blocked, false)){
+
+			// Do not build actions for widgets on the blacklist
+			// The blackListed widgets are those that have been filtered during the SPY mode with the
+			//CAPS_LOCK + SHIFT + Click clickfilter functionality.
+
+			//if (!blackListed(w)){
+
+			//For widgets that are:
+			// - clickable
+			// and
+			// - unFiltered by any of the regular expressions in the Filter-tab, or
+			// - whitelisted using the clickfilter functionality in SPY mode (CAPS_LOCK + SHIFT + CNTR + Click)
+			// We want to create actions that consist of left clicking on them
+
+			if(isClickable(w) /*&& (isUnfiltered(w) || whiteListed(w))*/) {
+				//Create a left click action with the Action Compiler, and add it to the set of derived actions
+				actions.add(ac.leftClickAt(w));
+			}
+
+			//For widgets that are:
+			// - typeable
+			// and
+			// - unFiltered by any of the regular expressions in the Filter-tab, or
+			// - whitelisted using the clickfilter functionality in SPY mode (CAPS_LOCK + SHIFT + CNTR + Click)
+			// We want to create actions that consist of typing into them
+
+			if(isTypeable(w) /*&& (isUnfiltered(w) || whiteListed(w))*/) {
+				//Create a type action with the Action Compiler, and add it to the set of derived actions
+				actions.add(ac.clickTypeInto(w, this.getRandomText(w), true));
+			}
+
+			//Add sliding actions (like scroll, drag and drop) to the derived actions
+			//method defined below.
+			addSlidingActions(actions,ac,SCROLL_ARROW_SIZE,SCROLL_THICK,w, state);
+			//}
+			//}
+		}
+
+		actions.clear();
+
+		System.out.println("IMAGE ITERATION");
+
+		if(!imageWidgets.isEmpty()) {
+			for (Entry<Rect, String> entry : imageWidgets.entrySet()) {
+				Rect key = entry.getKey();
+				String value = entry.getValue();
+
+				actions.add(ac.leftClickAt(key.x(), key.y()));
+
+				System.out.println("Rect: " + key + "   Text: " + value);
+			}
+
+		}
+
+		return actions;
+	}
+
+	/**
+	 * Select one of the available actions using an action selection algorithm (for example random action selection)
+	 *
+	 * super.selectAction(state, actions) updates information to the HTML sequence report
+	 *
+	 * @param state the SUT's current state
+	 * @param actions the set of derived actions
+	 * @return  the selected action (non-null!)
+	 */
+	@Override
+	protected Action selectAction(State state, Set<Action> actions){
+		return(super.selectAction(state, actions));
+	}
+
+	/**
+	 * Execute the selected action.
+	 *
+	 * super.executeAction(system, state, action) is updating the HTML sequence report with selected action
+	 *
+	 * @param system the SUT
+	 * @param state the SUT's current state
+	 * @param action the action to execute
+	 * @return whether or not the execution succeeded
+	 */
+	@Override
+	protected boolean executeAction(SUT system, State state, Action action){
+		return super.executeAction(system, state, action);
+	}
+
+	/**
+	 * TESTAR uses this method to determine when to stop the generation of actions for the
+	 * current sequence. You can stop deriving more actions after:
+	 * - a specified amount of executed actions, which is specified through the SequenceLength setting, or
+	 * - after a specific time, that is set in the MaxTime setting
+	 * @return  if <code>true</code> continue generation, else stop
+	 */
+	@Override
+	protected boolean moreActions(State state) {
+		return super.moreActions(state);
+	}
+
+
+	/**
+	 * TESTAR uses this method to determine when to stop the entire test sequence
+	 * You could stop the test after:
+	 * - a specified amount of sequences, which is specified through the Sequences setting, or
+	 * - after a specific time, that is set in the MaxTime setting
+	 * @return  if <code>true</code> continue test, else stop
+	 */
+	@Override
+	protected boolean moreSequences() {
+		return super.moreSequences();
+	}
+
+	/**
+	 * Here you can put graceful shutdown sequence for your SUT
+	 * @param system
+	 */
+	@Override
+	protected void stopSystem(SUT system) {
+		ScreenshotSerialiser.exit();
+		super.stopSystem(system);
+	}
+
+	/**
+	 * This methods is called after each test sequence, allowing for example using external profiling software on the SUT
+	 *
+	 * super.postSequenceProcessing() is adding test verdict into the HTML sequence report
+	 */
+	@Override
+	protected void postSequenceProcessing() {
+		super.postSequenceProcessing();
+	}
+}
