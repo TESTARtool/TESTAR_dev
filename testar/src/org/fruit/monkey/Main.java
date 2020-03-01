1a32950994b376b10281d97d3eaf9c94d26cc33e
Ad Mulders <admulders@gmail.com>
2020-03-01 12:14:07 +0100

    activating new test

--- testar/src/org/fruit/monkey/Main.java
+++ testar/src/org/fruit/monkey/Main.java
@@ -168,7 +168,7 @@ public class Main {
 			}
 
 			if (settings.get(InitTests)) {
-				sqlManager.initTest5(settings.get(ClearResults));
+				sqlManager.initTest3(settings.get(ClearResults));
 
 				if (settings.get(InitTestsOnly)) {
 					System.exit(1);
