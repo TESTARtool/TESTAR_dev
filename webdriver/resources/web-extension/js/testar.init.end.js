15daa59acd455192b8c7bbf03797864ebe2b5787
Fernando Pastor <32359126+ferpasri@users.noreply.github.com>
2020-03-12 17:23:16 +0100

    update js webdriver extension
    Chromedriver & Geckodriver:
    - "browser" is not a reserved word, then exists the possibility to find a web element with id="browser" that returns an existing web element object.
    This is going to be accepted by the first if condition, because a browser web element object exists, but how is it not the browser webextension API, the following browser.APIfunction will throw an error:
    (https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API)
    
    - Update deprecated "extension.getURL" to "runtime.getURL"
    https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/extension/getURL

--- webdriver/resources/web-extension/js/testar.init.end.js
+++ webdriver/resources/web-extension/js/testar.init.end.js
@@ -5,13 +5,13 @@
 
 var s1 = document.createElement('script');
 var s2 = document.createElement('script');
-if (typeof browser != 'undefined') {
-	s1.src = browser.extension.getURL('js/testar.state.js');
-	s2.src = browser.extension.getURL('js/testar.canvas.js');
+if (typeof chrome.runtime != 'undefined') {
+	s1.src = chrome.runtime.getURL('js/testar.state.js');
+	s2.src = chrome.runtime.getURL('js/testar.canvas.js');
 } 
 else {
-	s1.src = chrome.extension.getURL('js/testar.state.js');
-	s2.src = chrome.extension.getURL('js/testar.canvas.js');
+	s1.src = browser.runtime.getURL('js/testar.state.js');
+	s2.src = browser.runtime.getURL('js/testar.canvas.js');
 }
 (document.head || document.documentElement).appendChild(s1);
 (document.head || document.documentElement).appendChild(s2);
