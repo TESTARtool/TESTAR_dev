6eaf480fac9506b6f8a29313c99ed70761602baf
Ad Mulders <admulders@gmail.com>
2020-03-24 19:09:14 +0100

    disabled typing and sliding actions

--- testar/resources/settings/desktop_orientdb/Protocol_desktop_orientdb.java
+++ testar/resources/settings/desktop_orientdb/Protocol_desktop_orientdb.java
@@ -31,19 +31,13 @@
 import java.util.Set;
 import nl.ou.testar.RandomActionSelector;
 import org.fruit.Drag;
-import org.fruit.alayer.AbsolutePosition;
-import org.fruit.alayer.Point;
-import org.fruit.alayer.Action;
+import org.fruit.alayer.*;
 import org.fruit.alayer.exceptions.*;
-import org.fruit.alayer.SUT;
-import org.fruit.alayer.State;
-import org.fruit.alayer.Verdict;
-import org.fruit.alayer.Widget;
 import org.fruit.alayer.actions.AnnotatingActionCompiler;
 import org.fruit.alayer.actions.StdActionCompiler;
 import es.upv.staq.testar.protocols.ClickFilterLayerProtocol;
 import org.fruit.monkey.Settings;
-import org.fruit.alayer.Tags;
+
 import static org.fruit.alayer.Tags.Blocked;
 import static org.fruit.alayer.Tags.Enabled;
 import org.testar.protocols.DesktopProtocol;
@@ -70,6 +64,112 @@ public class Protocol_desktop_orientdb extends DesktopProtocol {
 			if (modelAction != null) return modelAction;
 			System.out.println("StateModelManager did not return an action. Returning random");
 			return RandomActionSelector.selectAction(actions);
+	}
+
+	@Override
+	protected Set<Action> deriveClickTypeScrollActionsFromAllWidgetsOfState(Set<Action> actions, SUT system, State state){
+		// To derive actions (such as clicks, drag&drop, typing ...) we should first create an action compiler.
+		StdActionCompiler ac = new AnnotatingActionCompiler();
+
+		// To find all possible actions that TESTAR can click on we should iterate through all widgets of the state.
+		for(Widget w : state){
+			//optional: iterate through top level widgets based on Z-index:
+			//for(Widget w : getTopWidgets(state)){
+
+			if(w.get(Tags.Role, Roles.Widget).toString().equalsIgnoreCase("UIAMenu")){
+				// filtering out actions on menu-containers (that would add an action in the middle of the menu)
+				continue; // skip this iteration of the for-loop
+			}
+
+			// Only consider enabled and non-blocked widgets
+			if(w.get(Enabled, true) && !w.get(Blocked, false)){
+
+				// Do not build actions for widgets on the blacklist
+				// The blackListed widgets are those that have been filtered during the SPY mode with the
+				//CAPS_LOCK + SHIFT + Click clickfilter functionality.
+				if (!blackListed(w)){
+
+					//For widgets that are:
+					// - clickable
+					// and
+					// - unFiltered by any of the regular expressions in the Filter-tab, or
+					// - whitelisted using the clickfilter functionality in SPY mode (CAPS_LOCK + SHIFT + CNTR + Click)
+					// We want to create actions that consist of left clicking on them
+					if(isClickable(w) && (isUnfiltered(w) || whiteListed(w))) {
+						//Create a left click action with the Action Compiler, and add it to the set of derived actions
+						actions.add(ac.leftClickAt(w));
+					}
+
+//					//For widgets that are:
+//					// - typeable
+//					// and
+//					// - unFiltered by any of the regular expressions in the Filter-tab, or
+//					// - whitelisted using the clickfilter functionality in SPY mode (CAPS_LOCK + SHIFT + CNTR + Click)
+//					// We want to create actions that consist of typing into them
+//					if(isTypeable(w) && (isUnfiltered(w) || whiteListed(w))) {
+//						//Create a type action with the Action Compiler, and add it to the set of derived actions
+//						actions.add(ac.clickTypeInto(w, this.getRandomText(w), true));
+//					}
+//					//Add sliding actions (like scroll, drag and drop) to the derived actions
+//					//method defined below.
+//					addSlidingActions(actions,ac,SCROLL_ARROW_SIZE,SCROLL_THICK,w, state);
+				}
+			}
+		}
+		return actions;
+	}
+
+	@Override
+	protected Set<Action> deriveClickTypeScrollActionsFromTopLevelWidgets(Set<Action> actions, SUT system, State state){
+		// To derive actions (such as clicks, drag&drop, typing ...) we should first create an action compiler.
+		StdActionCompiler ac = new AnnotatingActionCompiler();
+
+		// To find all possible actions that TESTAR can click on we should iterate through all widgets of the state.
+		for(Widget w : getTopWidgets(state)){
+			//optional: iterate through top level widgets based on Z-index:
+			//for(Widget w : getTopWidgets(state)){
+
+			if(w.get(Tags.Role, Roles.Widget).toString().equalsIgnoreCase("UIAMenu")){
+				// filtering out actions on menu-containers (that would add an action in the middle of the menu)
+				continue; // skip this iteration of the for-loop
+			}
+
+			// Only consider enabled and non-blocked widgets
+			if(w.get(Enabled, true) && !w.get(Blocked, false)){
+
+				// Do not build actions for widgets on the blacklist
+				// The blackListed widgets are those that have been filtered during the SPY mode with the
+				//CAPS_LOCK + SHIFT + Click clickfilter functionality.
+				if (!blackListed(w)){
+
+					//For widgets that are:
+					// - clickable
+					// and
+					// - unFiltered by any of the regular expressions in the Filter-tab, or
+					// - whitelisted using the clickfilter functionality in SPY mode (CAPS_LOCK + SHIFT + CNTR + Click)
+					// We want to create actions that consist of left clicking on them
+					if(isClickable(w) && (isUnfiltered(w) || whiteListed(w))) {
+						//Create a left click action with the Action Compiler, and add it to the set of derived actions
+						actions.add(ac.leftClickAt(w));
+					}
+
+//					//For widgets that are:
+//					// - typeable
+//					// and
+//					// - unFiltered by any of the regular expressions in the Filter-tab, or
+//					// - whitelisted using the clickfilter functionality in SPY mode (CAPS_LOCK + SHIFT + CNTR + Click)
+//					// We want to create actions that consist of typing into them
+//					if(isTypeable(w) && (isUnfiltered(w) || whiteListed(w))) {
+//						//Create a type action with the Action Compiler, and add it to the set of derived actions
+//						actions.add(ac.clickTypeInto(w, this.getRandomText(w), true));
+//					}
+//					//Add sliding actions (like scroll, drag and drop) to the derived actions
+//					//method defined below.
+//					addSlidingActions(actions,ac,SCROLL_ARROW_SIZE,SCROLL_THICK,w, state);
+				}
+			}
+		}
+		return actions;
 	}
 
 }
