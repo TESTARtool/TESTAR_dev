71245d873baf3a5ab87d3130344d8ce2cbe776cb
Arend Slomp <a.slomp@eagleconsultancy.com>
2020-09-27 14:00:08 +0200

    update compile_w10

--- windows/native_src/compile_w10.bat
+++ windows/native_src/compile_w10.bat
@@ -1,3 +1,3 @@
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" -arch=x64
 
 nmake -f Makefile_w10
