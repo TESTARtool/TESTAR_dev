1e084df6579a06d154b3613b98246d6e40006098
ferpasri <ferpasri@gmail.com>
2020-12-09 18:12:27 +0100

    remove bluej from suts

--- suts/bluej/bluej.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set APPBASE="C:\BlueJ\bluej"
-set CP=%APPBASE%\lib\bluej.jar;"C:\Program Files\Java\jdk1.8.0_181"\lib\tools.jar
-"C:\Program Files\Java\jdk1.8.0_181\bin\java" -cp %CP% bluej.Boot  %1 %2 %3 %4 %5 %6 %7 %8 %9
--- suts/bluej/lib/windowtofront.js
+++
@@ -1,4 +0,0 @@
-/* Brings a window to front on a Windows OS. */
-
-var shell = WScript.CreateObject("WScript.Shell");
-shell.AppActivate(WScript.Arguments.Item(0));
