6b7e4b622d3cf880c82660f7b1b0eee5533c5bf3
ferpasri <ferpasri@gmail.com>
2021-01-20 13:08:50 +0100

    RL settings structure
    After execute TESTAR, maintain the RL settings structure

--- testar/src/org/fruit/monkey/Settings.java
+++ testar/src/org/fruit/monkey/Settings.java
@@ -402,6 +402,20 @@ public class Settings extends TaggableBase implements Serializable {
 					+"AbstractStateAttributes =" + Util.lineSep()
 					+"\n"
 					+"#################################################################\n"
+					+"# Reinforcement learning settings\n"
+					+"#################################################################\n"
+					+"Alpha = " + Util.lineSep()
+					+"Gamma = " + Util.lineSep()
+					+"DefaultValue = " + Util.lineSep()
+					+"DefaultReward = " + Util.lineSep()
+					+"Epsilon = " + Util.lineSep()
+					+"MaxQValue = " + Util.lineSep()
+					+"DecayRate = " + Util.lineSep()
+					+"Temperature = " + Util.lineSep()
+					+"RewardFunction = " + Util.lineSep()
+					+"Policy = " + Util.lineSep()
+					+"\n"
+					+"#################################################################\n"
 					+"# Override display scale\n"
 					+"#\n"
 					+"# Overrides the displayscale obtained from the system.\n"
