747e2abba3ee6c1bce519e2f982820733ca2b7b7
Kacper Perz <kacper.perz@marviq.com>
2021-02-24 17:42:42 +0100

    IVS-33 Added logic for creation path to cloned directory

--- testar/src/org/fruit/monkey/vcs/GitServiceImpl.java
+++ testar/src/org/fruit/monkey/vcs/GitServiceImpl.java
@@ -5,17 +5,19 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class GitServiceImpl implements GitService {
 
-    private static final String LOCAL_REPOSITORIES_PATH = "";
+    private static final String LOCAL_REPOSITORIES_PATH = "cloned";
 
     @Override
     public boolean cloneRepository(String repositoryUrl) {
         try {
             Git.cloneRepository()
                     .setURI(repositoryUrl)
-                    .setDirectory(new File(LOCAL_REPOSITORIES_PATH))
+                    .setDirectory(prepareRepositoryDirectory(repositoryUrl))
                     .call();
             return true;
         } catch (GitAPIException e) {
@@ -29,12 +31,29 @@ public class GitServiceImpl implements GitService {
         try {
             Git.cloneRepository()
                     .setURI(repositoryUrl)
-                    .setDirectory(new File(LOCAL_REPOSITORIES_PATH))
+                    .setDirectory(prepareRepositoryDirectory(repositoryUrl))
                     .setCredentialsProvider(new UsernamePasswordCredentialsProvider(gitAuth.getUsername(), gitAuth.getPassword()))
                     .call();
             return true;
         } catch (GitAPIException e) {
             e.printStackTrace();
             return false;
-        }    }
+        }
+    }
+
+    private File prepareRepositoryDirectory(String repositoryUrl) {
+        Path baseDirPath = Paths.get(System.getProperty("user.dir"))
+                .getParent()
+                .resolve(LOCAL_REPOSITORIES_PATH);
+        Path repositoryPath = baseDirPath.resolve(extractRepositoryName(repositoryUrl));
+        File repositoryDirectory = new File(repositoryPath.toAbsolutePath().toString());
+        return repositoryDirectory;
+    }
+
+    private String extractRepositoryName(String repositoryUrl) {
+        String[] urlElements = repositoryUrl.split("/");
+        String repositoryNameWithExtension = urlElements[urlElements.length-1];
+        String repositoryName = repositoryNameWithExtension.split("\\.")[0];
+        return repositoryName;
+    }
 }
