e83f84675b2d2e98c0cc85497ebbd9182d584dd0
Nigel-Steijn <nigel.steijn@outlook.com>
2021-03-12 13:05:32 +0100

    Add test iterations to report

--- testar/src/nl/ou/testar/HtmlReporting/HtmlTestReport.java
+++ testar/src/nl/ou/testar/HtmlReporting/HtmlTestReport.java
@@ -5,45 +5,107 @@ import org.fruit.alayer.State;
 import org.fruit.alayer.Verdict;
 import org.testar.OutputStructure;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.stream.Stream;
 
 public class HtmlTestReport {
 
-    /** Directory that is used to store our template in */
+    /**
+     * Directory that is used to store our template in
+     */
     private static final String OUTPUT_DATA_DIR = "output/Testar-Report/";
-    /** Name of our report file */
+    /**
+     * Name of our report file
+     */
     private static final String HTML_INDEX_PATH = "report.html";
-    /** Location of our Issue Chart.js file */
+    /**
+     * Location of our Issue Chart.js file
+     */
     private static final String HTML_JS_ISSUE_CHART_PATH = "js/issueMetricChart.js";
-    /** Location of our Oracle Chart.js file */
+    /**
+     * Location of our Oracle Chart.js file
+     */
     private static final String HTML_JS_ORACLE_CHART_PATH = "js/oracleMetricChart.js";
-    /** Location of our CSS file */
+    /**
+     * Location of our CSS file
+     */
     private static final String HTML_CSS_REPORT_PATH = "css/report.css";
-    /** Content of the index template */
+    /**
+     * Content of the index template
+     */
     private String htmlIndex;
-    /** Content of our Issue chart.js template */
+    /**
+     * Content of our Issue chart.js template
+     */
     private String htmlJsIssueChart;
-    /** Content of our Oracle chart.js template */
+    /**
+     * Content of our Oracle chart.js template
+     */
     private String htmlJsOracleChart;
-    /** Content of our css template */
+    /**
+     * Content of our css template
+     */
     private String htmlCssReport;
 
-    /** Number of sequences executed */
+    /**
+     * Number of sequences executed
+     */
     private int sequences = 0;
-    /** Number of total actions executed */
+    /**
+     * Number of total actions executed
+     */
     private int actions = 0;
-    /** Number of non-severe issues found */
+    /**
+     * Number of non-severe issues found
+     */
     private int nonSevereIssues = 0;
-    /** Number of severe issues found */
+    /**
+     * Number of severe issues found
+     */
     private int severeIssues = 0;
 
-    /** Location where we should store our report in */
+    /**
+     * Location where we should store our report in
+     */
     private final String reportDir;
+
+    private class TestIteration {
+        private final int nonSevereIssues;
+        private final int severeIssues;
+        private final boolean hitOracle;
+
+        public TestIteration(int nonSevereIssues, int severeIssues) {
+            this.nonSevereIssues = nonSevereIssues;
+            this.severeIssues = severeIssues;
+            this.hitOracle = severeIssues > 0;
+        }
+
+        public int getNonSevereIssues() {
+            return this.nonSevereIssues;
+        }
+
+        public int getSevereIssues() {
+            return this.severeIssues;
+        }
+
+        public int getIssues() {
+            return this.severeIssues + this.nonSevereIssues;
+        }
+
+        public boolean hasHitOracle() {
+            return hitOracle;
+        }
+    }
+
+    private ArrayList<TestIteration> iterations;
 
     public HtmlTestReport() {
         /// Create a file name with the format: $outputDir/$dateTime_$sutName-report.html
@@ -55,6 +117,8 @@ public class HtmlTestReport {
                 OutputStructure.executedSUTname,
                 File.separator
         );
+
+        this.iterations = new ArrayList<>();
 
         try {
             // Read the report templates
@@ -73,11 +137,11 @@ public class HtmlTestReport {
         jsDirectory = new File(this.reportDir + "js/");
         cssDirectory = new File(this.reportDir + "css/");
 
-        if(!jsDirectory.exists())
+        if (!jsDirectory.exists())
             if (!jsDirectory.mkdirs())
                 System.err.println("Failed to create JS report directory!");
 
-        if(!cssDirectory.exists())
+        if (!cssDirectory.exists())
             if (!cssDirectory.mkdirs())
                 System.err.println("Failed to create CSS report directory!");
     }
@@ -95,6 +159,24 @@ public class HtmlTestReport {
     @FunctionalInterface
     private interface SaveFile<T> {
         void apply(T location, T content);
+    }
+
+    private String getIterationsAsHtml() {
+        StringBuilder sb = new StringBuilder("");
+
+        for (int i = 0; i < this.iterations.size(); i++) {
+            TestIteration it = this.iterations.get(i);
+            sb.append("<tr class=\"value\">\n");
+            sb.append(
+                    String.format(
+                            "<td>%d</td> <td>%d</td> <td>%s</td> <td> <button id=\"%s\" class=\"show-details\">Details</button> </td>\n",
+                            i, it.getIssues(), it.hasHitOracle() ? "True" : "False", "iteration_id_" + i
+                    )
+            );
+            sb.append("</tr>\n");
+        }
+
+        return sb.toString();
     }
 
     private String parseIndexTemplate(
@@ -116,7 +198,9 @@ public class HtmlTestReport {
                 .replace(
                         "<a href=\"https://www.example.com\">#[url]</a>",
                         String.format("<a href=\"%s\">%s</a>", url, title)
-                        );
+                )
+
+                .replace("#[iteration_trs]", this.getIterationsAsHtml());
     }
 
     public void saveReport(
@@ -156,5 +240,12 @@ public class HtmlTestReport {
 
     public void addTestVerdict(Verdict verdict) {
         this.sequences++;
+
+        this.iterations.add(
+                new TestIteration(
+                        this.nonSevereIssues,
+                        this.severeIssues
+                )
+        );
     }
 }
