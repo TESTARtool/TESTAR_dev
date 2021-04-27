184e7b8d4cfbf4289a2153c1d22d7517fb427bfd
Mark Dourlein <markdourlein@gmail.com>
2021-04-27 10:05:46 +0200

    Fixed null pointer counter based reward function

--- testar/src/nl/ou/testar/ReinforcementLearning/RewardFunctions/CounterBasedRewardFunction.java
+++ testar/src/nl/ou/testar/ReinforcementLearning/RewardFunctions/CounterBasedRewardFunction.java
@@ -4,25 +4,29 @@ import nl.ou.testar.ReinforcementLearning.RLTags;
 import nl.ou.testar.StateModel.AbstractAction;
 import nl.ou.testar.StateModel.AbstractState;
 import nl.ou.testar.StateModel.ConcreteState;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.fruit.alayer.State;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Implementation of the reward function based on a counter
  */
 public class CounterBasedRewardFunction implements RewardFunction {
 
-    private static final Logger logger = LoggerFactory.getLogger(CounterBasedRewardFunction.class);
+    private static final Logger logger = LogManager.getLogger(CounterBasedRewardFunction.class);
 
     @Override
     public float getReward(final State state, final ConcreteState currentConcreteState, final AbstractState currentAbstractState, final AbstractAction executedAction) {
+        if (executedAction == null || executedAction.getAttributes() == null) {
+            return 0f;
+        }
+
         int executionCounter = executedAction.getAttributes().get(RLTags.Counter, 0) + 1;
         executedAction.getAttributes().set(RLTags.Counter, executionCounter);
 
-        logger.debug("DEBUG: executionCounter={}", executionCounter);
+        logger.info("executionCounter={}", executionCounter);
         float reward = 1.0f / (float) executionCounter;
-        logger.debug("DEBUG: reward={}", reward);
+        logger.info("reward={}", reward);
 
         return reward;
     }
