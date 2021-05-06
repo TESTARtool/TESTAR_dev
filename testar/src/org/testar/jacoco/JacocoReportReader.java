f3c28e0f2ad9fa0d16c45b99fa5489385779c343
ferpasri <ferpasri@gmail.com>
2021-05-06 12:40:01 +0200

    update Jacoco coverage info

--- testar/src/org/testar/jacoco/JacocoReportReader.java
+++ testar/src/org/testar/jacoco/JacocoReportReader.java
@@ -1,7 +1,7 @@
 /***************************************************************************************************
  *
- * Copyright (c) 2020 Universitat Politecnica de Valencia - www.upv.es
- * Copyright (c) 2020 Open Universiteit - www.ou.nl
+ * Copyright (c) 2020 - 2021 Universitat Politecnica de Valencia - www.upv.es
+ * Copyright (c) 2020 - 2021 Open Universiteit - www.ou.nl
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -28,74 +28,123 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************************************/
 
-
 package org.testar.jacoco;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.text.NumberFormat;
+import java.util.Locale;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.parser.Parser;
-import org.jsoup.select.Elements;
-import org.testar.OutputStructure;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 
-import com.google.common.io.Files;
-
+/**
+ * Utility class for reading and extracting information from JaCoCo coverage reports.
+ */
 public class JacocoReportReader {
 
-	private String jacocoDirReport;
+    private JacocoReportReader() {}
 
-	public JacocoReportReader(String jacocoDirReport) {
-		this.jacocoDirReport = jacocoDirReport;
-	}
+    /**
+     * Read the CSV JaCoCo report and create the TOTAL metrics results. 
+     * 
+     * @param jacocoDirReport
+     * @return
+     */
+    public static String obtainCSVSummary(String jacocoDirReport) {
 
-	public String obtainHTMLSummary() {
+        File reportCSV = new File(jacocoDirReport + File.separator + "report_jacoco.csv");
 
-		File indexHTML = new File(jacocoDirReport + File.separator + "index.html");
+        //GROUP,      PACKAGE, CLASS,           INSTRUCTION_MISSED, INSTRUCTION_COVERED, BRANCH_MISSED, BRANCH_COVERED, LINE_MISSED, LINE_COVERED, COMPLEXITY_MISSED, COMPLEXITY_COVERED, METHOD_MISSED, METHOD_COVERED
+        //jacoco ant, default, FileChooserDemo, 191,                208,                 10,            0,              32,          43,           16,                6,                  11,            6
+        int[] totalValues = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // array for columns
 
-		try {
+        int numberOfTotalClasses = 0;
+        int numberOfMissedClasses = 0;
 
-			String htmlContent = Files.asCharSource(indexHTML, StandardCharsets.UTF_8).read();
+        try {
+            CSVReader reader = new CSVReader(new FileReader(reportCSV));
+            String[] nextLine;
 
-			Document doc = Jsoup.parse(htmlContent, "", Parser.xmlParser());
+            // Check and add all classes coverage values to get the TOTAL one
+            while((nextLine=reader.readNext()) != null) {
+                // extract values from columns 3 to 12
+                for(int i = 3; i <= 12; i++) {
+                    if(isIntValue(nextLine[i])) {
+                        totalValues[i] = totalValues[i] + Integer.parseInt(nextLine[i]);
+                    }
+                }
+                // Increase number of Total Classes if we have a numeric coverage line
+                if(isIntValue(nextLine[3])){
+                    numberOfTotalClasses = numberOfTotalClasses + 1;
+                }
+                // If INSTRUCTION_COVERED is 0, increase Missed Classes
+                if(isIntValue(nextLine[4]) && Integer.parseInt(nextLine[4]) == 0) {
+                    numberOfMissedClasses = numberOfMissedClasses + 1;
+                }
+            }
 
-			Elements coverageTable = doc.getElementsByTag("tfoot");
+            reader.close();
 
-			String tableResults = readCoverageTable(coverageTable);
+            int INSTRUCTION_MISSED = totalValues[3]; //column 3
+            int INSTRUCTION_COVERED = totalValues[4]; //column 4
+            int TOTAL_INSTRUCTIONS = INSTRUCTION_MISSED + INSTRUCTION_COVERED;
 
-			return tableResults;
+            int BRANCH_MISSED = totalValues[5]; //column 5
+            int BRANCH_COVERED = totalValues[6]; //column 6
+            int TOTAL_BRANCH = BRANCH_MISSED + BRANCH_COVERED;
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+            int LINE_MISSED = totalValues[7]; //column 7
+            int LINE_COVERED = totalValues[8]; //column 8
+            int TOTAL_LINE = LINE_MISSED + LINE_COVERED;
 
-		return "Summarized Coverage report not available";
-	}
+            int COMPLEXITY_MISSED = totalValues[9]; //column 9
+            int COMPLEXITY_COVERED = totalValues[10]; //column 10
+            int TOTAL_COMPLEXITY = COMPLEXITY_MISSED + COMPLEXITY_COVERED;
 
-	private String readCoverageTable(Elements coverageTable) {
-		Elements tableData = coverageTable.get(0).getElementsByTag("td");
+            int METHOD_MISSED = totalValues[11]; //column 11
+            int METHOD_COVERED = totalValues[12]; //column 12
+            int TOTAL_METHOD = METHOD_MISSED + METHOD_COVERED;
 
-		StringBuilder dataContent = new StringBuilder("");
+            StringBuilder dataContent = new StringBuilder("");
 
-		// Total | Missed Instructions | Cov. | Missed Branches | Cov. | Missed | Cxty | Missed | Lines | Missed | Methods | Missed | Classes
-		dataContent.append(OutputStructure.startInnerLoopDateString + "_" + OutputStructure.executedSUTname);
-		dataContent.append(": TOTAL | ");
-		dataContent.append("Missed Instructions: " + tableData.get(1).ownText() + " | ");
-		dataContent.append("Cov: " + tableData.get(2).ownText() + " | ");
-		dataContent.append("Missed Branches: " + tableData.get(3).ownText() + " | ");
-		dataContent.append("Cov: " + tableData.get(4).ownText() + " | ");
-		dataContent.append("Missed: " + tableData.get(5).ownText() + " | ");
-		dataContent.append("Cxty: " + tableData.get(6).ownText() + " | ");
-		dataContent.append("Missed: " + tableData.get(7).ownText() + " | ");
-		dataContent.append("Lines: " + tableData.get(8).ownText() + " | ");
-		dataContent.append("Missed: " + tableData.get(9).ownText() + " | ");
-		dataContent.append("Methods: " + tableData.get(10).ownText() + " | ");
-		dataContent.append("Missed: " + tableData.get(11).ownText() + " | ");
-		dataContent.append("Classes: " + tableData.get(12).ownText());
+            dataContent.append("Missed Instructions | " + NumberFormat.getNumberInstance(Locale.US).format(INSTRUCTION_MISSED));
+            dataContent.append(" of " + NumberFormat.getNumberInstance(Locale.US).format(TOTAL_INSTRUCTIONS));
+            dataContent.append(" | Cov | " + String.format("%.2f", (double)INSTRUCTION_COVERED * 100.0 / (double)TOTAL_INSTRUCTIONS).replace(".", ","));
 
-		return dataContent.toString();
-	}
+            dataContent.append(" | Missed Branches | " + NumberFormat.getNumberInstance(Locale.US).format(BRANCH_MISSED));
+            dataContent.append(" of " + NumberFormat.getNumberInstance(Locale.US).format(TOTAL_BRANCH));
+            dataContent.append(" | Cov | " + String.format("%.2f", (double)BRANCH_COVERED * 100.0 / (double)TOTAL_BRANCH).replace(".", ","));
 
+            dataContent.append(" | Missed | " + NumberFormat.getNumberInstance(Locale.US).format(COMPLEXITY_MISSED));
+            dataContent.append(" | Cxty | " + NumberFormat.getNumberInstance(Locale.US).format(TOTAL_COMPLEXITY));
+
+            dataContent.append(" | Missed | " + NumberFormat.getNumberInstance(Locale.US).format(LINE_MISSED));
+            dataContent.append(" | Lines | " + NumberFormat.getNumberInstance(Locale.US).format(TOTAL_LINE));
+
+            dataContent.append(" | Missed | " + NumberFormat.getNumberInstance(Locale.US).format(METHOD_MISSED));
+            dataContent.append(" | Methods | " + NumberFormat.getNumberInstance(Locale.US).format(TOTAL_METHOD));
+
+            dataContent.append(" | Missed | " + numberOfMissedClasses);
+            dataContent.append(" | Classes | " + numberOfTotalClasses);
+
+            return dataContent.toString();
+
+        } catch (CsvValidationException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return "Summarized Coverage CSV report not available";
+    }
+
+    private static boolean isIntValue(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch(NumberFormatException  e) {
+            // Exception, nothing but we return false
+        }
+        return false;
+    }
 }
