cbaad255c49364d202ca63a9051ea2f3bce088be
ferpasri <ferpasri@gmail.com>
2021-05-27 09:58:04 +0200

    extra functions updates
    - Update Readme to update gradlew instructions and docker image reference
    - Update workflow CI protocol, to test new webdriver tag name functions feature

--- testar/src/org/testar/protocols/GenericUtilsProtocol.java
+++ testar/src/org/testar/protocols/GenericUtilsProtocol.java
@@ -32,9 +32,9 @@
 package org.testar.protocols;
 
 import es.upv.staq.testar.NativeLinker;
+import es.upv.staq.testar.OperatingSystems;
 import es.upv.staq.testar.protocols.ClickFilterLayerProtocol;
 import nl.ou.testar.DerivedActions;
-import nl.ou.testar.StateModel.Persistence.OrientDB.Entity.Config;
 import org.fruit.Drag;
 import org.fruit.Util;
 import org.fruit.alayer.*;
@@ -65,7 +65,7 @@ public class GenericUtilsProtocol extends ClickFilterLayerProtocol {
      * @return
      */
     protected boolean waitAndLeftClickWidgetWithMatchingTag(String tagName, String value, State state, SUT system, int maxNumberOfRetries, double waitBetween){
-        if(settings.get(ConfigTags.SUTConnector).equalsIgnoreCase("WEB_DRIVER")){
+        if(NativeLinker.getPLATFORM_OS().contains(OperatingSystems.WEBDRIVER)){
             if(!tagName.startsWith("Web")){
                 tagName = "Web"+tagName;
             }
@@ -75,23 +75,23 @@ public class GenericUtilsProtocol extends ClickFilterLayerProtocol {
                 return waitAndLeftClickWidgetWithMatchingTag(tag,value,state,system,maxNumberOfRetries,waitBetween);
             }
         }
+        System.out.println("Matching widget was not found, "+tagName+"=" + value);
         return false;
     }
 
-
-        /**
-         * This method waits until the widget with a matching Tag value (case sensitive) is found or the retry limit is reached.
-         * If a matching widget is found, left mouse button is clicked on it and return value is true.
-         * Else returns false
-         *
-         * @param tag for example: org.fruit.alayer.Tags.Title
-         * @param value
-         * @param state
-         * @param system needed for updating the state between retries
-         * @param maxNumberOfRetries int number of times
-         * @param waitBetween double in seconds
-         * @return
-         */
+    /**
+     * This method waits until the widget with a matching Tag value (case sensitive) is found or the retry limit is reached.
+     * If a matching widget is found, left mouse button is clicked on it and return value is true.
+     * Else returns false
+     *
+     * @param tag for example: org.fruit.alayer.Tags.Title
+     * @param value
+     * @param state
+     * @param system needed for updating the state between retries
+     * @param maxNumberOfRetries int number of times
+     * @param waitBetween double in seconds
+     * @return
+     */
     protected boolean waitAndLeftClickWidgetWithMatchingTag(Tag<?> tag, String value, State state, SUT system, int maxNumberOfRetries, double waitBetween){
         int numberOfRetries = 0;
         while(numberOfRetries<maxNumberOfRetries){
@@ -129,7 +129,7 @@ public class GenericUtilsProtocol extends ClickFilterLayerProtocol {
      * @return
      */
     protected boolean waitLeftClickAndTypeIntoWidgetWithMatchingTag(String tagName, String value, String textToType, State state, SUT system, int maxNumberOfRetries, double waitBetween){
-        if(settings.get(ConfigTags.SUTConnector).equalsIgnoreCase("WEB_DRIVER")){
+        if(NativeLinker.getPLATFORM_OS().contains(OperatingSystems.WEBDRIVER)){
             if(!tagName.startsWith("Web")){
                 tagName = "Web"+tagName;
             }
@@ -139,24 +139,25 @@ public class GenericUtilsProtocol extends ClickFilterLayerProtocol {
                 return waitLeftClickAndTypeIntoWidgetWithMatchingTag(tag,value,textToType,state,system,maxNumberOfRetries,waitBetween);
             }
         }
+        System.out.println("Matching widget was not found, "+tagName+"=" + value);
         return false;
     }
 
 
-        /**
-         * This method waits until the widget with a matching Tag value (case sensitive) is found or the retry limit is reached.
-         * If a matching widget is found, left mouse button is clicked on it, the given text is typed into it, and return value is true.
-         * Else returns false
-         *
-         * @param tag for example: org.fruit.alayer.Tags.Title
-         * @param value
-         * @param textToType types the given text by replacing the existing text
-         * @param state
-         * @param system needed for updating the state between retries
-         * @param maxNumberOfRetries int number of times
-         * @param waitBetween double in seconds
-         * @return
-         */
+    /**
+     * This method waits until the widget with a matching Tag value (case sensitive) is found or the retry limit is reached.
+     * If a matching widget is found, left mouse button is clicked on it, the given text is typed into it, and return value is true.
+     * Else returns false
+     *
+     * @param tag for example: org.fruit.alayer.Tags.Title
+     * @param value
+     * @param textToType types the given text by replacing the existing text
+     * @param state
+     * @param system needed for updating the state between retries
+     * @param maxNumberOfRetries int number of times
+     * @param waitBetween double in seconds
+     * @return
+     */
     protected boolean waitLeftClickAndTypeIntoWidgetWithMatchingTag(Tag<?> tag, String value, String textToType, State state, SUT system, int maxNumberOfRetries, double waitBetween){
         int numberOfRetries = 0;
         while(numberOfRetries<maxNumberOfRetries){
@@ -194,7 +195,7 @@ public class GenericUtilsProtocol extends ClickFilterLayerProtocol {
      * @return
      */
     protected boolean waitLeftClickAndPasteIntoWidgetWithMatchingTag(String tagName, String value, String textToPaste, State state, SUT system, int maxNumberOfRetries, double waitBetween){
-        if(settings.get(ConfigTags.SUTConnector).equalsIgnoreCase("WEB_DRIVER")){
+        if(NativeLinker.getPLATFORM_OS().contains(OperatingSystems.WEBDRIVER)){
             if(!tagName.startsWith("Web")){
                 tagName = "Web"+tagName;
             }
@@ -204,44 +205,45 @@ public class GenericUtilsProtocol extends ClickFilterLayerProtocol {
                 return waitLeftClickAndPasteIntoWidgetWithMatchingTag(tag,value,textToPaste,state,system,maxNumberOfRetries,waitBetween);
             }
         }
+        System.out.println("Matching widget was not found, "+tagName+"=" + value);
         return false;
     }
 
 
-        /**
-         * This method waits until the widget with a matching Tag value (case sensitive) is found or the retry limit is reached.
-         * If a matching widget is found, left mouse button is clicked on it, the given text is pasted into it, and return value is true.
-         * Else returns false
-         *
-         * @param tag for example: org.fruit.alayer.Tags.Title
-         * @param value
-         * @param textToPaste paste the given text by replacing the existing text
-         * @param state
-         * @param system needed for updating the state between retries
-         * @param maxNumberOfRetries int number of times
-         * @param waitBetween double in seconds
-         * @return
-         */
+    /**
+     * This method waits until the widget with a matching Tag value (case sensitive) is found or the retry limit is reached.
+     * If a matching widget is found, left mouse button is clicked on it, the given text is pasted into it, and return value is true.
+     * Else returns false
+     *
+     * @param tag for example: org.fruit.alayer.Tags.Title
+     * @param value
+     * @param textToPaste paste the given text by replacing the existing text
+     * @param state
+     * @param system needed for updating the state between retries
+     * @param maxNumberOfRetries int number of times
+     * @param waitBetween double in seconds
+     * @return
+     */
     protected boolean waitLeftClickAndPasteIntoWidgetWithMatchingTag(Tag<?> tag, String value, String textToPaste, State state, SUT system, int maxNumberOfRetries, double waitBetween){
-    	int numberOfRetries = 0;
-    	while(numberOfRetries<maxNumberOfRetries){
-    		//looking for a widget with matching tag value:
-    		Widget widget = getWidgetWithMatchingTag(tag,value,state);
-    		if(widget!=null){
-    			StdActionCompiler ac = new AnnotatingActionCompiler();
-    			executeAction(system, state, ac.pasteTextInto(widget, textToPaste, true));
-    			// is waiting needed after the action has been executed?
-    			return true;
-    		}
-    		else{
-    			Util.pause(waitBetween);
-    			state = getState(system);
-    			numberOfRetries++;
-    		}
-    	}
-    	System.out.println("Matching widget was not found, "+tag.toString()+"=" + value);
-    	printTagValuesOfWidgets(tag,state);
-    	return false;
+        int numberOfRetries = 0;
+        while(numberOfRetries<maxNumberOfRetries){
+            //looking for a widget with matching tag value:
+            Widget widget = getWidgetWithMatchingTag(tag,value,state);
+            if(widget!=null){
+                StdActionCompiler ac = new AnnotatingActionCompiler();
+                executeAction(system, state, ac.pasteTextInto(widget, textToPaste, true));
+                // is waiting needed after the action has been executed?
+                return true;
+            }
+            else{
+                Util.pause(waitBetween);
+                state = getState(system);
+                numberOfRetries++;
+            }
+        }
+        System.out.println("Matching widget was not found, "+tag.toString()+"=" + value);
+        printTagValuesOfWidgets(tag,state);
+        return false;
     }
 
     /**
@@ -254,7 +256,7 @@ public class GenericUtilsProtocol extends ClickFilterLayerProtocol {
      * @return
      */
     protected Widget getWidgetWithMatchingTag(String tagName, String value, State state){
-        if(settings.get(ConfigTags.SUTConnector).equalsIgnoreCase("WEB_DRIVER")){
+        if(NativeLinker.getPLATFORM_OS().contains(OperatingSystems.WEBDRIVER)){
             if(!tagName.startsWith("Web")){
                 tagName = "Web"+tagName;
             }
@@ -267,15 +269,15 @@ public class GenericUtilsProtocol extends ClickFilterLayerProtocol {
         return null;
     }
 
-        /**
-         * Iterates the widgets of the state until a widget with matching tag value is found.
-         * The value is case sensitive.
-         *
-         * @param tag
-         * @param value
-         * @param state
-         * @return the matching widget if found, null if not found
-         */
+    /**
+     * Iterates the widgets of the state until a widget with matching tag value is found.
+     * The value is case sensitive.
+     *
+     * @param tag
+     * @param value
+     * @param state
+     * @return the matching widget if found, null if not found
+     */
     protected Widget getWidgetWithMatchingTag(Tag<?> tag, String value, State state){
         for(Widget widget:state){
             if(widget.get(tag, null)==null){
@@ -290,7 +292,6 @@ public class GenericUtilsProtocol extends ClickFilterLayerProtocol {
         }
         return null;
     }
-
 
     /**
      * Prints to system out all the widgets that have some value in the given tag.
@@ -308,7 +309,6 @@ public class GenericUtilsProtocol extends ClickFilterLayerProtocol {
             }
         }
     }
-
 
     /**
      * Adds sliding actions (like scroll, drag and drop) to the given Set of Actions
@@ -330,7 +330,7 @@ public class GenericUtilsProtocol extends ClickFilterLayerProtocol {
                         new AbsolutePosition(Point.from(drag.getFromX(),drag.getFromY())),
                         new AbsolutePosition(Point.from(drag.getToX(),drag.getToY())),
                         widget
-                ));
+                        ));
 
             }
         }
@@ -353,17 +353,17 @@ public class GenericUtilsProtocol extends ClickFilterLayerProtocol {
      */
     protected DerivedActions addSlidingActions(DerivedActions derived, StdActionCompiler ac, Drag[] drags, Widget widget){
 
-            //TODO creates multiple drag actions for one widget?
-            //For each possible drag, create an action and add it to the derived actions
-            for (Drag drag : drags){
-                //Create a slide action with the Action Compiler, and add it to the set of derived actions
-                derived.addAvailableAction(ac.slideFromTo(
-                        new AbsolutePosition(Point.from(drag.getFromX(),drag.getFromY())),
-                        new AbsolutePosition(Point.from(drag.getToX(),drag.getToY())),
-                        widget
-                ));
+        //TODO creates multiple drag actions for one widget?
+        //For each possible drag, create an action and add it to the derived actions
+        for (Drag drag : drags){
+            //Create a slide action with the Action Compiler, and add it to the set of derived actions
+            derived.addAvailableAction(ac.slideFromTo(
+                    new AbsolutePosition(Point.from(drag.getFromX(),drag.getFromY())),
+                    new AbsolutePosition(Point.from(drag.getToX(),drag.getToY())),
+                    widget
+                    ));
 
-            }
+        }
         return derived;
     }
 
