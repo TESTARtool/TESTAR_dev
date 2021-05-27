3f7f80b662d7c5f61857e068de88539f0c005339
pekkakaho <33083138+pekkakaho@users.noreply.github.com>
2021-05-27 13:58:07 +0300

    adding an example of triggered compound action
    adding an example of triggered compound action for Parabank protocol

--- testar/resources/settings/02_webdriver_parabank/Protocol_webdriver_parabank.java
+++ testar/resources/settings/02_webdriver_parabank/Protocol_webdriver_parabank.java
@@ -181,6 +181,49 @@ public class Protocol_webdriver_parabank extends WebdriverProtocol {
     // Check if forced actions are needed to stay within allowed domains
     Set<Action> forcedActions = detectForcedActions(state, ac);
 
+    // Add triggered actions here, before deriving the actions in a normal way:
+      /*
+      //Check if the trigger element is found:
+      Widget triggerWidget = getWidgetWithMatchingTag("name","payee.name", state);
+      if(triggerWidget!=null){
+          // The element was found, create the triggered action and return it:
+          // Creating a builder for a compound action that includes multiple actions as one item:
+          CompoundAction.Builder multiAction = new CompoundAction.Builder();
+          // Creating an action to type text into the Payee Name field:
+          multiAction.add(ac.clickTypeInto(triggerWidget, "Triggered Payer Name", true),1.0);
+          // Creating an action to type text into name="payee.address.street":
+          multiAction.add(ac.clickTypeInto(getWidgetWithMatchingTag("name","payee.address.street", state),
+                  "Triggered Payer Street", true),1.0);
+          // Creating an action to type text into name="payee.address.city":
+          multiAction.add(ac.clickTypeInto(getWidgetWithMatchingTag("name","payee.address.city", state),
+                  "Triggered City", true),1.0);
+          // Creating an action to type text into name="payee.address.state":
+          multiAction.add(ac.clickTypeInto(getWidgetWithMatchingTag("name","payee.address.state", state),
+                  "Triggered State", true),1.0);
+          // Creating an action to type text into name="payee.zipCode":
+          multiAction.add(ac.clickTypeInto(getWidgetWithMatchingTag("name","payee.address.zipCode", state),
+                  "12345", true),1.0);
+          // Creating an action to type text into name="payee.phoneNumber":
+          multiAction.add(ac.clickTypeInto(getWidgetWithMatchingTag("name","payee.phoneNumber", state),
+                  "123456789", true),1.0);
+          // Creating an action to type text into name="payee.accountNumber":
+          multiAction.add(ac.clickTypeInto(getWidgetWithMatchingTag("name","payee.accountNumber", state),
+                  "12341234", true),1.0);
+          // Creating an action to type text into name="verifyAccount":
+          multiAction.add(ac.clickTypeInto(getWidgetWithMatchingTag("name","verifyAccount", state),
+                  "12341234", true),1.0);
+          // Creating an action to type text into name="amount":
+          multiAction.add(ac.clickTypeInto(getWidgetWithMatchingTag("name","amount", state),
+                  "100", true),1.0);
+          // Creating a click action on Send Payment button, <input type="submit" class="button" value="Send Payment">
+          multiAction.add(ac.leftClickAt(getWidgetWithMatchingTag("value","Send Payment", state)),1.0);
+          // Adding the compound action into the actions that will be returned:
+          actions.add(multiAction.build());
+          // Returning actions having only the triggered action, before the normal derive actions:
+          return actions;
+      }
+*/
+
     // iterate through all widgets
     for (Widget widget : state) {
 
