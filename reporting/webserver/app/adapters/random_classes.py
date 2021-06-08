e964ec17ad6890f21bb6fa207dc8a2d4c119177c
Arjan de Haan <arjan.de.haan@mail.local>
2021-06-08 17:08:54 +0200

    Implement test data model build upon

---
+++ reporting/webserver/app/adapters/random_classes.py
@@ -0,0 +1,154 @@
+from datetime import datetime
+from typing import List
+import numpy as np
+
+from .abstract_classes import (
+    AbstractAction, AbstractReport, AbstractSequence, AbstractVerdict)
+
+# Example for fake data generation
+URLS = ['https://example.com', 'https://www.ou.nl/', 'https://testar.org/',
+        'https://duckduckgo.com/', 'http://github.com/', 'https://marviq.com/']
+SEED = 0
+WORDS = ['lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'sed',
+         'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna', 'aliqua']
+
+
+class Verdict(AbstractVerdict):
+
+    def __init__(self, id: int):
+        super().__init__()
+        self._id = id
+        self._random = np.random.RandomState(id)
+
+        # Set internal variables
+        self._severity = self._random.random()
+
+    def get_id(self) -> int:
+        return self._id
+
+    def get_tag(self) -> str:
+        pass
+
+    def get_severity(self) -> float:
+        return self._severity
+
+
+class Action(AbstractAction):
+
+    def __init__(self, id: int):
+        super().__init__()
+        self._id = id
+        self._random = np.random.RandomState(id)
+
+        # Generate random datetime
+        self._started = datetime.fromtimestamp(self._random.randint(20e7,20e8))
+
+        # Generate description
+        temp = []
+        for _ in range(self._random.randint(3, 15)):
+            temp.append(self._random.choice(WORDS))
+        self._description = ' '.join(temp)
+
+        # Generate status
+        temp = []
+        for _ in range(self._random.randint(1, 5)):
+            temp.append(self._random.choice(WORDS))
+        self._status = ' '.join(temp)
+
+    def get_screenshot(self) -> str:
+        return '/static/screenshot/test.png'
+
+    def get_description(self) -> str:
+        return self._description
+
+    def get_status(self) -> str:
+        return self._status
+
+    def get_started(self) -> datetime:
+        return self._started
+
+    def get_id(self) -> int:
+        return self._id
+
+
+class Sequence(AbstractSequence):
+
+    def __init__(self, id: int, max_actions: int):
+        super().__init__()
+        self._random = np.random.RandomState(id)
+        self._id = id
+
+        # Generate actions
+        self._actions = []
+        for _ in range(self._random.randint(1, max_actions)):
+            action = Action(self._random.randint(0, 1e5))
+            self._actions.append(action)
+
+        self._verdicts = []
+        # Random chance of generating no verdicts
+        if bool(self._random.rand(1) >= 0.75):
+
+            # Generate verdicts
+            for _ in range(self._random.randint(1, 3)):
+                verdict = Verdict(self._random.randint(0, 1e5))
+                self._verdicts.append(verdict)
+
+    def get_actions(self) -> List[Action]:
+        return self._actions
+
+    def get_verdicts(self) -> List[Verdict]:
+        return self._verdicts
+
+    def has_verdicts(self) -> bool:
+        return bool(self._verdicts)
+
+    def get_id(self) -> int:
+        return self._id
+
+
+class Report(AbstractReport):
+
+    def __init__(self, id: int):
+        super().__init__()
+        self._id = id
+        self._random = np.random.RandomState(id)
+        self._sequences = []
+        self._url = self._random.choice(URLS)
+        self._actions_per_sequence = self._random.randint(5, 100)
+
+        # Generate a random amount of sequences
+        for _ in range(self._random.randint(0, 25)):
+            sequence = Sequence(self._random.randint(
+                0, 1e5), self._actions_per_sequence)
+            self._sequences.append(sequence)
+
+    def get_sequences(self) -> List[Sequence]:
+        return self._sequences
+
+    def get_url(self) -> str:
+        return self._url
+
+    def get_actions_per_sequence(self) -> int:
+        return self._actions_per_sequence
+
+    def get_id(self) -> int:
+        return self._id
+
+    def get_sequence_by_id(self, id) -> Sequence:
+        return Sequence(id, self._actions_per_sequence)
+
+    @classmethod
+    def get_reports(cls) -> list:
+        np.random.seed(SEED)
+        report_count = np.random.randint(0, 25)
+        reports = []
+
+        # Generate a random amount of reports
+        for _ in range(report_count):
+            report_id = np.random.randint(0, 1e5)
+            reports.append(Report(report_id))
+        return reports
+
+    @classmethod
+    def get_report_by_id(cls, id: int):
+        return Report(id)
