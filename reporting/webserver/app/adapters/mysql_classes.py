6b06be35b4a1ce44300beb0f83043c5eada3a4fc
Arjan de Haan <arjan.de.haan@mail.local>
2021-06-09 15:11:18 +0200

    Implemented stub mysql adapter

---
+++ reporting/webserver/app/adapters/mysql_classes.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from typing import List
+from mysql.connector import connect, Error
+
+from abc import ABC, abstractclassmethod, abstractmethod
+
+# TODO Implement this
