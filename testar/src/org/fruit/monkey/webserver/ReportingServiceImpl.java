7aa0d5de8e122672872c270b162eef71bc63a64f
Michael Tikhonenko <michael.tikhonenko@marviq.com>
2021-06-18 11:23:30 +0200

    IVS-88 Outputs mounted to a webserver to display screenshots

--- testar/src/org/fruit/monkey/webserver/ReportingServiceImpl.java
+++ testar/src/org/fruit/monkey/webserver/ReportingServiceImpl.java
@@ -11,11 +11,13 @@ public class ReportingServiceImpl implements ReportingService {
 
     private DockerPoolService dockerPoolService;
     private final String imageId;
-    private File webserverDir;
+    private final File webserverDir;
+    private final File outputDir;
 
     public ReportingServiceImpl(DockerPoolService dockerPoolService) throws IOException {
         this.dockerPoolService = dockerPoolService;
         webserverDir = new File(Main.webserverDir);
+        outputDir = new File(Main.outputDir);
         imageId = dockerPoolService.buildImage(webserverDir, null);
     }
 
@@ -23,7 +25,10 @@ public class ReportingServiceImpl implements ReportingService {
     public void start(int port, String dbHostname, int dbPort, String dbName, String dbUsername, String dbPassword) throws IOException {
         final HostConfig hostConfig = HostConfig.newHostConfig()
                 .withPortBindings(PortBinding.parse(String.format("%d:80", port)))
-                .withBinds(new Bind(webserverDir.getAbsolutePath(), new Volume("/app")));
+                .withBinds(
+                        new Bind(webserverDir.getAbsolutePath(), new Volume("/app")),
+                        new Bind(outputDir.getAbsolutePath(), new Volume("/static/output"))
+                );
         final String[] env = {"ADAPTER=MYSQL", "MYSQL_HOST=" + dbHostname, "MYSQL_PORT=" + dbPort,
             "MYSQL_DATABASE=" + dbName,  "MYSQL_USER=" + dbUsername, "MYSQL_PASSWORD=" + dbPassword,
             "MYSQL_WAIT=5"};
