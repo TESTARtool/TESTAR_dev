90f5e0ff041603125c2c7dc0010c64e869173a2b
Michael Tikhonenko <michael.tikhonenko@marviq.com>
2021-06-18 11:00:55 +0200

    IVS-88 Reporting web service now works, still need to show screenshots

---
+++ testar/resources/extras/webserver/app/views.py
@@ -0,0 +1,33 @@
+import os
+from app import app
+from flask import render_template
+
+if os.environ.get('ADAPTER') == 'RANDOM':
+    from .adapters.random_classes import Report
+elif os.environ.get('ADAPTER') == 'MYSQL':
+    from .adapters.mysql_classes import Report, setup_db_pool
+    setup_db_pool()
+else:
+    raise Exception('No adapter set')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', reports=Report.get_reports())
+
+
+@app.route('/report/<int:id>/')
+def report_page(id):
+    report = Report(id)
+    return render_template('report.html', report=report)
+
+
+@app.route('/sequence/<int:report_id>/<int:sequence_id>')
+def sequence(report_id, sequence_id):
+
+    # Retrieve report
+    report = Report(report_id)
+
+    # Retrieve given sequence
+    sequence = report.get_sequence_by_id(sequence_id)
+    return render_template('details.html', report=report, sequence=sequence)
---
+++ testar/resources/extras/webserver/main.py
@@ -0,0 +1,3 @@
+from app import app
+
+# WSGI application runs on this
