c594872cb6d5f1af4e7aaec2c936b39b10ec75d7
Michael Tikhonenko <michael.tikhonenko@marviq.com>
2021-06-21 09:52:55 +0200

    IVS-88 Reporting webservice relocated

--- testar/src/org/fruit/monkey/Main.java
+++ testar/src/org/fruit/monkey/Main.java
@@ -94,7 +94,7 @@ public class Main extends Application implements DashboardDelegate, ProtocolDele
 	//Extras
 	public static String extrasDir = testarDir + "extras" + File.separator;
 	public static String databaseDir = extrasDir + "database" + File.separator;
-	public static String webserverDir = extrasDir + "webserver" + File.separator;
+	public static String webserverDir = extrasDir + "reporting" + "webserver" + File.separator;
 	public static String sonarqubeDir = extrasDir + "sonarqube";
 	public static String sonarqubeClientDir = extrasDir + "sonarqube_client";
 
