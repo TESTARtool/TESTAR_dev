9ed74f6a7882c522fd19e6b2138cf62d288aa257
Michael Tikhonenko <michael.tikhonenko@marviq.com>
2021-08-04 18:27:37 +0200

    JavaFX migration in progress

---
+++ testar/src/org/fruit/monkey/jfx/SettingsDialogJFX.java
@@ -0,0 +1,11 @@
+package org.fruit.monkey.jfx;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+
+public class SettingsDialogJFX extends Scene {
+    public SettingsDialogJFX(Parent root) {
+        super(root);
+    }
+}
