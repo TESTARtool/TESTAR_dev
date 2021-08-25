0bf52b4ff4920f5bde321e4260f0b0de5b45d7af
Michael Tikhonenko <michael.tikhonenko@marviq.com>
2021-08-25 08:28:30 +0200

    Settings screen now can have multiple sections

--- testar/src/nl/ou/testar/jfx/settings/child/GeneralSettingsController.java
+++ testar/src/nl/ou/testar/jfx/settings/child/GeneralSettingsController.java
@@ -1,7 +1,21 @@
 package nl.ou.testar.jfx.settings.child;
 
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
 public class GeneralSettingsController extends ChildSettingsController {
     public GeneralSettingsController() {
-        super("General settings", "jfx/settings_general.fxml");
+        super("General settings");
+    }
+
+    @Override
+    public void viewDidLoad(Parent view) {
+        super.viewDidLoad(view);
+        try {
+            putSection(view, "General settings", "jfx/settings_general.fxml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- testar/src/nl/ou/testar/jfx/settings/child/TimeSettingsController.java
+++ testar/src/nl/ou/testar/jfx/settings/child/TimeSettingsController.java
@@ -1,7 +1,21 @@
 package nl.ou.testar.jfx.settings.child;
 
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
 public class TimeSettingsController extends ChildSettingsController {
     public TimeSettingsController() {
-        super("Time settings", "jfx/settings_time.fxml");
+        super("Time settings");
+    }
+
+    @Override
+    public void viewDidLoad(Parent view) {
+        super.viewDidLoad(view);
+        try {
+            putSection(view, "Time settings", "jfx/settings_time.fxml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
