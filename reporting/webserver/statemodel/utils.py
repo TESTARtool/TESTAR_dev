ac9d9566f5a99fd1d94d4aa583c3e26854d1c100
Arjan de Haan <agdehaan@gmail.com>
2021-09-01 13:45:36 +0200

    Implemented A* path finding.

--- reporting/webserver/statemodel/utils.py
+++ reporting/webserver/statemodel/utils.py
@@ -11,12 +11,12 @@ class OrientDB:
         return self._client.query(command, **kwargs)
 
     def concrete_states(self) -> list:
-        states = self.query("SELECT * FROM ConcreteState")
+        states = self.query('SELECT format("%s", @rid), * FROM ConcreteState')
         
         return [self._state_to_object(x) for x in states]
 
     def concrete_state_from_cid(self, cid):
-        states = self.query(f'SELECT * FROM ConcreteState WHERE ConcreteID="{cid}"')
+        states = self.query(f'SELECT format("%s", @rid), * FROM ConcreteState WHERE ConcreteID="{cid}"')
 
         # Verify if the state exists
         if not states:
@@ -24,35 +24,59 @@ class OrientDB:
 
         return self._state_to_object(states[0])
 
-    def _is_valid_edge(self, edge):
-        if 'ConcreteID' not in edge._OrientRecord__o_storage:
-            return False
-        else:
-            return True
+    def concrete_state_traverse(self, cid_from: str, cid_to: str):
+        """Attempt to find the shortest to traverse between 2 concrete states.
+
+        Args:
+            cid_from (str): Path starting location.
+            cid_to (str): Path goal location.
+
+        Returns:
+            list: Steps of concrete states to reach the given concrete state.
+            None:  When one of the given states is not found.
+        """
+        state_from = self.concrete_state_from_cid(cid_from)
+        state_to = self.concrete_state_from_cid(cid_to)
+
+        # Returns none when one of the given states is not found.
+        if state_from is not None and state_to is not None:
+            return None
+
+        rid_from = state_from['rid']
+        rid_to = state_to['rid']
+
+        query = f"SELECT format(\"%s\", @rid), * FROM (SELECT flatten(ASTAR) FROM (SELECT ASTAR({rid_from}, {rid_to}, 'ConcreteAction', {{'direction': 'IN', 'parallel' : 1 }}) FROM ConcreteState LIMIT 1))"
+        vertex_traversed = self.query(query)
+        print(query, vertex_traversed)
+        return [self._state_to_object(x) for x in vertex_traversed]
 
     def _edge_to_object(self, uid):
-        edge = self.query(f'SELECT out.ConcreteID, in.ConcreteID, Shape, ConcreteID, `Desc`, ValuePattern FROM ConcreteAction WHERE uid="{uid}";')[0]
+        edge = self.query(f'SELECT format("%s", @rid), out.ConcreteID, in.ConcreteID, in.oracleVerdictCode , Shape, ConcreteID, `Desc`, ValuePattern FROM ConcreteAction WHERE uid="{uid}";')[0]
 
-        shape_source = edge.oRecordData['Shape'] if 'Shape' in edge.oRecordData else edge._OrientRecord__o_storage['WebBoundingRectangle']
+        if not 'Shape' in edge.oRecordData:
+            return
+
+        shape_source = edge.oRecordData['Shape']
         shape = [int(float(x.group())) for x in re.finditer(r"\d*\.0", shape_source)]
         
         return {    
-            "class_type" : edge.oRecordData,
+            "rid" : edge.format,
             "shape" : shape,
             "description" : edge.oRecordData['Desc'],
             "fromState": edge.oRecordData['out'],
             "toState": edge.oRecordData['in'],
             "value": edge.oRecordData['ValuePattern'],
-            "ConcreteID": edge.oRecordData['ConcreteID']
+            "ConcreteID": edge.oRecordData['ConcreteID'],
+            "toOracle": edge.oRecordData['in2'] != 1
 
         }
 
     def _state_to_object(self, state):
-        rid = state._OrientRecord__rid
+        rid = state.format
 
         # Retrieve edge orient objects
         edges_in = self.query(
-                f'SELECT uid FROM (TRAVERSE inE() FROM {rid}) WHERE @class="ConcreteAction"'
+                f'SELECT uid FROM (TRAVERSE inE() FROM {rid}) WHERE @class="ConcreteAction" '
             )
         edges_out = self.query(
                 f'SELECT uid FROM (TRAVERSE outE() FROM {rid}) WHERE @class="ConcreteAction"'
