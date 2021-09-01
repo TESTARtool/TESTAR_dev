22c163429e30ec52dcff3bd5ddc2984fb5016842
Arjan de Haan <agdehaan@gmail.com>
2021-09-01 13:45:15 +0200

    Renamed urls & added traverse api

--- reporting/webserver/statemodel/views.py
+++ reporting/webserver/statemodel/views.py
@@ -11,23 +11,31 @@ statemodel = Blueprint(
 
 orientdb = OrientDB.from_env()
 
-@statemodel.route("/concrete/state")
+@statemodel.route("/concrete/")
 async def concrete_states_list():
     states = orientdb.concrete_states()
     return render_template("concrete_state_list.html", states=states)
 
 
-@statemodel.route("/concrete/state/<string:cid>")
+@statemodel.route("/concrete/<string:cid>")
 async def concrete_state_details(cid):
     state = orientdb.concrete_state_from_cid(cid)
     return render_template("concrete_state_detail.html", state=state)
 
+@statemodel.route('/concrete/traverse/<string:from_cid>/<string:to_cid>')
+async def concrete_state_traverse(from_cid, to_cid):
+    pass # TODO: Implement this
+
 ############## API #############
 
-@statemodel.route("/api/concrete/state/<string:cid>")
+@statemodel.route("/api/concrete/<string:cid>")
 async def json_concrete_state_details(cid):
     return jsonify(orientdb.concrete_state_from_cid(cid))
 
-@statemodel.route("/api/concrete/state")
+@statemodel.route("/api/concrete/")
 async def json_concrete_state_list():
     return  jsonify(orientdb.concrete_states())
+
+@statemodel.route("/api/concrete/traverse/<string:from_cid>/<string:to_cid>")
+async def json_state_traverse(from_cid, to_cid):
+    return jsonify(orientdb.concrete_state_traverse(from_cid, to_cid))
