d7c9c81d05ba1571c2524f479f3bc0043221c730
Michael Tikhonenko <michael.tikhonenko@marviq.com>
2021-09-07 10:59:45 +0200

    Startup settings should use tables

--- testar/src/nl/ou/testar/jfx/settings/child/MiscSettingsController.java
+++ testar/src/nl/ou/testar/jfx/settings/child/MiscSettingsController.java
@@ -1,6 +1,15 @@
 package nl.ou.testar.jfx.settings.child;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.fruit.Pair;
+import org.fruit.monkey.ConfigTags;
 import org.fruit.monkey.Settings;
 
 import java.io.IOException;
@@ -19,5 +28,22 @@ public class MiscSettingsController extends ChildSettingsController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        TableView<Pair<String, String>> copyTable = (TableView) view.lookup("#copyTable");
+
+        TableColumn<Pair<String, String>, String> copyFromColumn = (TableColumn<Pair<String, String>, String>) copyTable.getColumns().get(0);
+        TableColumn<Pair<String, String>, String> copyToColumn = (TableColumn<Pair<String, String>, String>) copyTable.getColumns().get(1);
+        copyFromColumn.setCellValueFactory(data -> new ReadOnlyObjectWrapper<>(data.getValue().left()));
+        copyToColumn.setCellValueFactory(data -> new ReadOnlyObjectWrapper<>(data.getValue().right()));
+
+
+        copyTable.getItems().addAll(settings.get(ConfigTags.CopyFromTo));
+
+        TableView<String> deleteTable = (TableView) view.lookup("#deleteTable");
+
+        TableColumn<String, String> deleteColumn = (TableColumn<String, String>) deleteTable.getColumns().get(0);
+        deleteColumn.setCellValueFactory(data -> new ReadOnlyObjectWrapper<String>(data.getValue()));
+
+        deleteTable.getItems().addAll(settings.get(ConfigTags.Delete));
     }
 }
