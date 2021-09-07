698a165ecdff03190a089eb0c328e62e2633abcf
Arjan de Haan <agdehaan@gmail.com>
2021-09-07 11:16:57 +0200

    Implemented reporting service builder

--- testar/src/org/fruit/monkey/webserver/ReportingService.java
+++ testar/src/org/fruit/monkey/webserver/ReportingService.java
@@ -2,8 +2,8 @@ package org.fruit.monkey.webserver;
 
 import java.io.IOException;
 
-public interface ReportingService {
+public interface  ReportingService {
     ReportingServiceDelegate getDelegate();
     void setDelegate(ReportingServiceDelegate delegate);
-    void start(int port, String dbHostname, int dbPort, String dbName, String dbUsername, String dbPassword) throws IOException;
+    void start() throws IOException;
 }
