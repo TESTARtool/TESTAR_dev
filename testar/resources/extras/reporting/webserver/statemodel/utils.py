f15cb7149efb2e5da6f03a0b8dfa3863cdcaf32f
Arjan de Haan <agdehaan@gmail.com>
2021-10-13 15:17:41 +0200

    Refactored reporting to use external orientdb

--- testar/resources/extras/reporting/webserver/statemodel/utils.py
+++ testar/resources/extras/reporting/webserver/statemodel/utils.py
@@ -1,159 +1,153 @@
 import os
 import re
-import pyorient
+from external import OrientDB
 
-class OrientDB:
-    def __init__(self, host: str, port: int, user: str, database: str, password: str):
-        self._client = pyorient.OrientDB(host, port)
-        self.database = self._client.db_open(database, user, password)
-
-    def query(self, command, **kwargs) -> list:
-        return self._client.query(command, **kwargs)
-
-    def concrete_states(self) -> list:
-        states = self.query('SELECT format("%s", @rid), * FROM ConcreteState')
-        
-        return self._states_to_object(states)
-
-    def concrete_state_from_cid(self, cid):
-        states = self.query(f'SELECT format("%s", @rid), * FROM ConcreteState WHERE ConcreteID="{cid}"')
-
-        # Verify if the state exists
-        if not states:
-            return None
-
-        return self._state_to_object(states[0])
-
-    def concrete_state_traverse(self, cid_from: str, cid_to: str):
-        """Attempt to find the shortest to traverse between 2 concrete states.
-
-        Args:
-            cid_from (str): Path starting location.
-            cid_to (str): Path goal location.
-
-        Returns:
-            list: Steps of concrete states to reach the given concrete state.
-            None:  When one of the given states is not found.
-        """
-        state_from = self.concrete_state_from_cid(cid_from)
-        state_to = self.concrete_state_from_cid(cid_to)
-
-        # Returns none when one of the given states is not found.
-        if state_from is None or state_to is None:
-            return None
-
-        rid_from = state_from['rid']
-        rid_to = state_to['rid']
-
-        query = f"SELECT format(\"%s\", @rid), * FROM (SELECT flatten(ASTAR) FROM (SELECT ASTAR({rid_from}, {rid_to}, 'ConcreteAction', {{'direction': 'OUT', 'parallel' : 1 }}) FROM ConcreteState LIMIT 1))"
-        vertex_traversed = self.query(query)
-        return [self._state_to_object(x) for x in vertex_traversed]
-
-    def _edge_to_object(self, uid):
-        edge = self.query(f'SELECT format("%s", @rid), out.ConcreteID, in.ConcreteID, in.oracleVerdictCode , Shape, ConcreteID, `Desc`, ValuePattern FROM ConcreteAction WHERE uid="{uid}";')
-
-        # Select first if it exists or return nothing
-        if edge:
-            edge = edge[0]
-        else:
-            return
-
-        if not 'Shape' in edge.oRecordData:
-            return
-
-        shape_source = edge.oRecordData['Shape']
-        shape = [int(float(x.group())) for x in re.finditer(r"\d*\.0", shape_source)]
-        
-        return {    
-            "rid" : edge.format,
-            "shape" : shape,
-            "description" : edge.oRecordData['Desc'],
-            "fromState": edge.oRecordData['out'],
-            "toState": edge.oRecordData['in'],
-            "value": edge.oRecordData['ValuePattern'],
-            "ConcreteID": edge.oRecordData['ConcreteID'],
-            "toOracle": edge.oRecordData['in2'] != 1
-
-        }
-
-    def _edges_to_object(self, edges):
-        """Convert a list of edges uids to a list of edge objects.
-
-        Args:
-            edges (list<uid>): A list with edge uids
-        """
-        output = []
-        for edge in edges:
-
-            # Extract uid from OrientRecord
-            if hasattr(edge, 'oRecordData'):
-                edge = edge.oRecordData['uid']
-
-            edge_object = self._edge_to_object(edge)
-
-            # Ignore empty edges
-            if edge_object is None:
-                continue
-
-            output.append(edge_object)
-        return output
-
-    def _states_to_object(self, states):
-        """Convert a list of oRecord states to a list of state objects.
-
-        Args:
-           states (list<oRecordData>): A list with edge uids
-        """
-        output = []
-        for state in states:
-
-            state_object = self._state_to_object(state)
-
-            # Ignore empty edges
-            if state_object is None:
-                continue
-
-            output.append(state_object)
-        return output
+orientdb = OrientDB()
 
 
-    def _state_to_object(self, state):
-        rid = state.format
+def concrete_states() -> list:
+    states = orientdb.query('SELECT format("%s", @rid), * FROM ConcreteState')
 
-        # Retrieve edge orient objects
-        edges_in = self.query(
-                f'SELECT uid FROM (TRAVERSE inE() FROM {rid}) WHERE @class="ConcreteAction" '
-            )
-        edges_out = self.query(
-                f'SELECT uid FROM (TRAVERSE outE() FROM {rid}) WHERE @class="ConcreteAction"'
-            )
+    return _states_to_object(states)
 
-        # Convert to dicts
-        edges_in = self._edges_to_object(edges_in)
-        edges_out = self._edges_to_object(edges_out)
-        
-        # Extract shape
-        if not hasattr(state,'Shape'):
-            return 
-        
-        shape = [int(float(x.group())) for x in re.finditer(r"\d*\.0", state.Shape)]
 
-        return {
-            "cid": state.ConcreteID,
-            "rid": rid,
-            "screenshot": '/static' + state.ScreenshotPath[1:],
-            "shape": shape,
-            "description": state.Desc,
-            "edges_out": edges_out,
-            "edges_in": edges_in,
-            "oracle": state.oRecordData['oracleVerdictCode'] != 1
-        }
+def concrete_state_from_cid(cid):
+    states = orientdb.query(
+        f'SELECT format("%s", @rid), * FROM ConcreteState WHERE ConcreteID="{cid}"'
+    )
 
-    @classmethod
-    def from_env(cls):
-        host = os.environ.get("ORIENTDB_HOST", "orientdb")
-        user = os.environ.get("ORIENTDB_USER", "root")
-        password = os.environ.get("ORIENTDB_PASSWORD", "testar")
-        database = os.environ.get("ORIENTDB_DATABASE", "testar")
-        port = int(os.environ.get("ORIENTDB_PORT", 2424))
-        return cls(host, port, user, database, password)
+    # Verify if the state exists
+    if not states:
+        return None
+
+    return _state_to_object(states[0])
+
+
+def concrete_state_traverse(cid_from: str, cid_to: str):
+    """Attempt to find the shortest to traverse between 2 concrete states.
+
+    Args:
+        cid_from (str): Path starting location.
+        cid_to (str): Path goal location.
+
+    Returns:
+        list: Steps of concrete states to reach the given concrete state.
+        None:  When one of the given states is not found.
+    """
+    state_from = concrete_state_from_cid(cid_from)
+    state_to = concrete_state_from_cid(cid_to)
+
+    # Returns none when one of the given states is not found.
+    if state_from is None or state_to is None:
+        return None
+
+    rid_from = state_from["rid"]
+    rid_to = state_to["rid"]
+
+    query = "SELECT format('%s', @rid), * FROM (SELECT flatten(ASTAR) FROM (SELECT ASTAR(?, ?, 'ConcreteAction', {'direction': 'OUT', 'parallel' : 1 }) FROM ConcreteState LIMIT 1))"
+    vertex_traversed = orientdb.query(query, rid_from, rid_to)
+    return [_state_to_object(x) for x in vertex_traversed]
+
+
+def _edge_to_object(uid: str):
+    query = "SELECT format('%s', @rid), out.ConcreteID, in.ConcreteID, in.oracleVerdictCode , `Shape`, `ConcreteID`, `Desc`, `ValuePattern` FROM ConcreteAction WHERE uid='?';"
+    edge = orientdb.query(query, uid)
+    print(edge, orientdb._sanitize(query, uid), flush=True)
+    # Select first if it exists or return nothing
+    if edge:
+        edge = edge[0]
+    else:
+        return
+
+    if not "Shape" in edge.oRecordData:
+        return
+
+    shape_source = edge.oRecordData["Shape"]
+    shape = [int(float(x.group())) for x in re.finditer(r"\d*\.0", shape_source)]
+
+    return {
+        "rid": edge.format,
+        "shape": shape,
+        "description": edge.oRecordData["Desc"],
+        "fromState": edge.oRecordData["out"],
+        "toState": edge.oRecordData["in"],
+        "value": edge.oRecordData["ValuePattern"],
+        "ConcreteID": edge.oRecordData["ConcreteID"],
+        "toOracle": edge.oRecordData["in2"] != 1,
+    }
+
+
+def _edges_to_object(edges):
+    """Convert a list of edges uids to a list of edge objects.
+
+    Args:
+        edges (list<uid>): A list with edge uids
+    """
+    output = []
+
+    for edge in edges:
+
+        # Extract uid from OrientRecord
+        if hasattr(edge, "oRecordData"):
+            edge = edge.oRecordData["uid"]
+
+        edge_object = _edge_to_object(edge)
+
+        # Ignore empty edges
+        if edge_object is None:
+            continue
+
+        output.append(edge_object)
+    return output
+
+
+def _states_to_object(states):
+    """Convert a list of oRecord states to a list of state objects.
+
+    Args:
+        states (list<oRecordData>): A list with edge uids
+    """
+    output = []
+    for state in states:
+
+        state_object = _state_to_object(state)
+
+        # Ignore empty edges
+        if state_object is None:
+            continue
+
+        output.append(state_object)
+    return output
+
+
+def _state_to_object(state):
+    rid = state.format
+
+    # Retrieve edge orient objects
+    edges_in = orientdb.query(
+        "SELECT `uid` FROM (TRAVERSE inE() FROM ?) WHERE @class='ConcreteAction'", rid
+    )
+    edges_out = orientdb.query(
+        "SELECT `uid` FROM (TRAVERSE outE() FROM ?) WHERE @class='ConcreteAction'", rid
+    )
+
+    # Convert to dicts
+    edges_in = _edges_to_object(edges_in)
+    edges_out = _edges_to_object(edges_out)
+
+    # Extract shape
+    if not hasattr(state, "Shape"):
+        return
+
+    shape = [int(float(x.group())) for x in re.finditer(r"\d*\.0", state.Shape)]
+
+    return {
+        "cid": state.ConcreteID,
+        "rid": rid,
+        "screenshot": "/static" + state.ScreenshotPath[1:],
+        "shape": shape,
+        "description": state.Desc,
+        "edges_out": edges_out,
+        "edges_in": edges_in,
+        "oracle": state.oRecordData["oracleVerdictCode"] != 1,
+    }
--- testar/resources/extras/reporting/webserver/statemodel/views.py
+++ testar/resources/extras/reporting/webserver/statemodel/views.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, render_template, jsonify
-from statemodel.utils import OrientDB
+from statemodel.utils import (
+    concrete_states,
+    concrete_state_from_cid,
+    concrete_state_traverse,
+)
 
 statemodel = Blueprint(
     "statemodel",
@@ -9,50 +13,56 @@ statemodel = Blueprint(
     static_url_path="/static/statemodel",
 )
 
-orientdb = OrientDB.from_env()
 
 @statemodel.route("/concrete/")
-async def concrete_states_list():
-    states = orientdb.concrete_states()
+async def view_concrete_states_list():
+    states = concrete_states()
     return render_template("concrete_state_list.html", states=states)
 
 
 @statemodel.route("/concrete/<string:cid>")
-async def concrete_state_details(cid):
-    state = orientdb.concrete_state_from_cid(cid)
+async def view_concrete_state_details(cid):
+    state = concrete_state_from_cid(cid)
     return render_template("concrete_state_detail.html", state=state)
 
-@statemodel.route('/concrete/traverse/<string:from_cid>/<string:to_cid>')
-async def concrete_state_traverse(from_cid, to_cid):
-    return render_template("concrete_state_traverse.html", from_cid=from_cid, to_cid=to_cid)
+
+@statemodel.route("/concrete/traverse/<string:from_cid>/<string:to_cid>")
+async def view_concrete_state_traverse(from_cid, to_cid):
+    return render_template(
+        "concrete_state_traverse.html", from_cid=from_cid, to_cid=to_cid
+    )
+
 
 ############## API #############
 
+
 @statemodel.route("/api/concrete/<string:cid>")
 async def json_concrete_state_details(cid):
-    state = orientdb.concrete_state_from_cid(cid)
+    state = concrete_state_from_cid(cid)
 
     if state:
         return jsonify(state)
 
-    return jsonify({'message': 'State not found or misses information.'})
+    return jsonify({"message": "State not found or misses information."})
+
 
 @statemodel.route("/api/concrete/")
 async def json_concrete_state_list():
-    state = orientdb.concrete_states()
+    state = concrete_states()
     if state:
         return jsonify(state)
     else:
-        return jsonify({'message': 'No states found.'})
+        return jsonify({"message": "No states found."})
+
 
 @statemodel.route("/api/concrete/traverse/<string:from_cid>/<string:to_cid>")
 async def json_state_traverse(from_cid, to_cid):
 
     # Don't request items with the same ConcreteID
     if from_cid == to_cid:
-        return jsonify({'message': 'from_cid & to_cid are the same'})
+        return jsonify({"message": "from_cid & to_cid are the same"})
 
-    path = orientdb.concrete_state_traverse(from_cid, to_cid)
+    path = concrete_state_traverse(from_cid, to_cid)
 
     # Only return path when it exists
     if path:
@@ -60,4 +70,4 @@ async def json_state_traverse(from_cid, to_cid):
 
     # Return error message if there is no path between 2 states
     else:
-        return jsonify({'message': f'No path found from `{from_cid}` to `{to_cid}`.'})
+        return jsonify({"message": f"No path found from `{from_cid}` to `{to_cid}`."})
