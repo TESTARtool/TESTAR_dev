ffc0c688a0859b962f30ee8eb042059e1dbf7a86
ferpasri <ferpasri@gmail.com>
2021-11-25 15:03:18 +0100

    Type same text in ReplayModel mode

--- testar/src/org/testar/protocols/WebdriverProtocol.java
+++ testar/src/org/testar/protocols/WebdriverProtocol.java
@@ -353,7 +353,7 @@ public class WebdriverProtocol extends GenericUtilsProtocol {
         String status = "";
         String statusInfo = "";
 
-        if(mode() == Modes.Replay) {
+        if(mode() == Modes.Replay || mode() == Modes.ReplayModel) {
             htmlReport.addTestVerdict(getReplayVerdict().join(processVerdict));
             status = (getReplayVerdict().join(processVerdict)).verdictSeverityTitle();
             statusInfo = (getReplayVerdict().join(processVerdict)).info();
