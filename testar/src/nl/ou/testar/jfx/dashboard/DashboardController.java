4df3c4287d022f2f4467b8968f8bdde2add9a326
Ardolynk <ardolynk75@gmail.com>
2022-03-17 14:37:11 +0100

    IVS-188 JavaFX error messaging

--- testar/src/nl/ou/testar/jfx/dashboard/DashboardController.java
+++ testar/src/nl/ou/testar/jfx/dashboard/DashboardController.java
@@ -65,6 +65,7 @@ public class DashboardController extends ViewController {
 
     private void startWhiteboxTesting(Parent view) {
         final Stage stage = (Stage) view.getScene().getWindow();
+        final WhiteboxTestLauncher testStatus = new WhiteboxTestLauncher();
 
         final JfxProgressMonitor progressMonitor = new JfxProgressMonitor();
         final WhiteboxTestLauncher whiteboxTestLauncher = new WhiteboxTestLauncher(progressMonitor);
