f40342158055b47ff007d03d9fde7d5096ccf6ac
Fernando Pastor <32359126+ferpasri@users.noreply.github.com>
2022-06-21 09:18:41 +0200

    Update TESTAR version

--- testar/src/org/testar/settingsdialog/SettingsDialog.java
+++ testar/src/org/testar/settingsdialog/SettingsDialog.java
@@ -68,7 +68,7 @@ import static javax.swing.UIManager.*;
 public class SettingsDialog extends JFrame implements Observer {
   private static final long serialVersionUID = 5156320008281200950L;
 
-  static final String TESTAR_VERSION = "2.4.3 (26-Apr-2022)";
+  static final String TESTAR_VERSION = "2.5.0 (21-Jun-2022)";
 
   private String settingsFile;
   private Settings settings;
