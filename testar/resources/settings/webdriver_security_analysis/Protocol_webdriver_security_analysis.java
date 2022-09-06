f2f4ca51170bb8a53dd97b9bc8a4a8e5477dcbcb
Fernando Pastor <32359126+ferpasri@users.noreply.github.com>
2022-09-06 13:31:26 +0200

    Copyright and output results update
    - Add copyright to securityanalysis files
    - Add OWASP SUT info to the webdriver_security_analysis protocol
    - Modify the output path of the security results to the TESTAR run output folder

--- testar/resources/settings/webdriver_security_analysis/Protocol_webdriver_security_analysis.java
+++ testar/resources/settings/webdriver_security_analysis/Protocol_webdriver_security_analysis.java
@@ -1,3 +1,33 @@
+/**
+ * Copyright (c) 2018 - 2022 Open Universiteit - www.ou.nl
+ * Copyright (c) 2018 - 2022 Universitat Politecnica de Valencia - www.upv.es
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 import org.apache.commons.lang.NotImplementedException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -7,6 +37,7 @@ import org.openqa.selenium.devtools.v101.network.Network;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testar.SutVisualization;
+import org.testar.monkey.Settings;
 import org.testar.monkey.alayer.*;
 import org.testar.monkey.alayer.actions.*;
 import org.testar.monkey.alayer.exceptions.ActionBuildException;
@@ -23,12 +54,32 @@ import org.testar.securityanalysis.helpers.SecurityOracleOrchestrator;
 
 import java.util.*;
 
+/**
+ * We recommend running this example of security analysis protocol with the OWASP benchmark SUT. 
+ * https://github.com/OWASP-Benchmark/BenchmarkJava 
+ * https://owasp.org/www-project-benchmark/ 
+ * 
+ * Required Java 8
+ * git clone https://github.com/OWASP-Benchmark/BenchmarkJava
+ * Execute runBenchmark.bat or runBenchmark.sh to deploy the vulnerable SUT example:
+ * https://localhost:8443/benchmark/
+ * 
+ * Other websites that offer web vulnerabilities examples
+ * https://www.acunetix.com/blog/web-security-zone/test-xss-skills-vulnerable-sites/
+ */
 public class Protocol_webdriver_security_analysis extends WebdriverProtocol {
     private SecurityResultWriter securityResultWriter;
     private NavigationHelper navigationHelper;
     private RemoteWebDriver webDriver;
     private SecurityOracleOrchestrator oracleOrchestrator;
     private SecurityConfiguration securityConfiguration = new SecurityConfiguration();
+
+    @Override
+    protected void initialize(Settings settings) {
+    	super.initialize(settings);
+    	// Disable web security such as "ignore-certificate-errors" for local testing purposes
+    	//WdDriver.disableSecurity = true;
+    }
 
     @Override
     protected void preSequencePreparations() {
