8dfd5194ed4c938831860685681faf6df7fb3656
oliviarves <olivia.rves@gmail.com>
2023-03-07 16:08:25 +0100

    Add ReinforcementLearningSettings to ExtendedSettingsFactory

--- testar/src/org/testar/extendedsettings/ExtendedSettingsFactory.java
+++ testar/src/org/testar/extendedsettings/ExtendedSettingsFactory.java
@@ -29,7 +29,7 @@
  *******************************************************************************************************/
 
 package org.testar.extendedsettings;
-
+import org.testar.reinforcementlearning.*;
 import org.testar.visualvalidation.VisualValidationSettings;
 
 import java.util.ArrayList;
@@ -79,6 +79,10 @@ public class ExtendedSettingsFactory {
 
     public static VisualValidationSettings createVisualValidationSettings() {
         return createSettings(VisualValidationSettings.class, VisualValidationSettings::CreateDefault);
+    }
+
+    public static ReinforcementLearningSettings createReinforcementLearningSettings() {
+        return createSettings(ReinforcementLearningSettings.class, ReinforcementLearningSettings::CreateDefault);
     }
 
     public static ExampleSetting createTestSetting() {
