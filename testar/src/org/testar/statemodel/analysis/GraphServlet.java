d5d4e34df0f5d353a1df00017bbb8df9fc28f773
Fernando Pastor <32359126+ferpasri@users.noreply.github.com>
2023-03-14 22:57:06 +0100

    Do not thrown servlet exceptions

--- testar/src/org/testar/statemodel/analysis/GraphServlet.java
+++ testar/src/org/testar/statemodel/analysis/GraphServlet.java
@@ -1,3 +1,33 @@
+/***************************************************************************************************
+ *
+ * Copyright (c) 2020 - 2023 Universitat Politecnica de Valencia - www.upv.es
+ * Copyright (c) 2020 - 2023 Open Universiteit - www.ou.nl
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************************************/
+
 package org.testar.statemodel.analysis;
 
 import javax.servlet.RequestDispatcher;
@@ -8,75 +38,77 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class GraphServlet  extends HttpServlet {
+public class GraphServlet extends HttpServlet {
 
-    protected void doPost(
-            HttpServletRequest request,
-            HttpServletResponse response)
-            throws ServletException, IOException {
+	private static final long serialVersionUID = -5154913154801901022L;
 
-        boolean abstractLayerRequired, concreteLayerRequired, sequenceLayerRequired, showCompoundGraph = false;
-        abstractLayerRequired = request.getParameter("abstractoption") != null;
-        concreteLayerRequired = request.getParameter("concreteoption") != null;
-        sequenceLayerRequired = request.getParameter("sequenceoption") != null;
-        showCompoundGraph = request.getParameter("compoundoption") != null;
-        String modelIdentifier = request.getParameter("modelIdentifier");
-        String concreteStateIdentifier = request.getParameter("concrete_state_id");
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+		boolean abstractLayerRequired, concreteLayerRequired, sequenceLayerRequired, showCompoundGraph = false;
+		abstractLayerRequired = request.getParameter("abstractoption") != null;
+		concreteLayerRequired = request.getParameter("concreteoption") != null;
+		sequenceLayerRequired = request.getParameter("sequenceoption") != null;
+		showCompoundGraph = request.getParameter("compoundoption") != null;
+		String modelIdentifier = request.getParameter("modelIdentifier");
+		String concreteStateIdentifier = request.getParameter("concrete_state_id");
 
-        // fetch the analysismanager from the servlet context
-        ServletContext servletContext = getServletContext();
-        // todo: this needs proper error handling
-        AnalysisManager analysisManager = (AnalysisManager)servletContext.getAttribute("analysisManager");
+		// fetch the analysismanager from the servlet context
+		ServletContext servletContext = getServletContext();
+		// todo: this needs proper error handling
+		AnalysisManager analysisManager = (AnalysisManager)servletContext.getAttribute("analysisManager");
 
-        if (modelIdentifier != null) {
-            // this is the controller logic for the overall model graph
+		if (modelIdentifier != null) {
+			// this is the controller logic for the overall model graph
 
-            // check if there were any layers requested
-            if (!(abstractLayerRequired || concreteLayerRequired || sequenceLayerRequired)) {
-                response.sendRedirect("/models");
-            }
+			// check if there were any layers requested
+			if (!(abstractLayerRequired || concreteLayerRequired || sequenceLayerRequired)) {
+				try {
+					response.sendRedirect("/models");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 
-            // fetch the model
-            String jsonFileName = analysisManager.fetchGraphForModel(modelIdentifier, abstractLayerRequired, concreteLayerRequired, sequenceLayerRequired, showCompoundGraph);
+			// fetch the model
+			String jsonFileName = analysisManager.fetchGraphForModel(modelIdentifier, abstractLayerRequired, concreteLayerRequired, sequenceLayerRequired, showCompoundGraph);
 
-            try {
-                request.setAttribute("graphContentFile", jsonFileName);
-                request.setAttribute("contentFolder", modelIdentifier);
-                RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/graph.jsp");
-                dispatcher.forward(request, response);
-            } catch (ServletException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        else if (concreteStateIdentifier != null) {
-            // this is the controller logic for the widget tree graph.
-            // if more parts are added, we should separate this logic into separate controllers
+			try {
+				request.setAttribute("graphContentFile", jsonFileName);
+				request.setAttribute("contentFolder", modelIdentifier);
+				RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/graph.jsp");
+				dispatcher.forward(request, response);
+			} catch (ServletException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		else if (concreteStateIdentifier != null) {
+			// this is the controller logic for the widget tree graph.
+			// if more parts are added, we should separate this logic into separate controllers
 
-            // fetch the model
-            String jsonFileName = analysisManager.fetchWidgetTree(concreteStateIdentifier);
+			// fetch the model
+			String jsonFileName = analysisManager.fetchWidgetTree(concreteStateIdentifier);
 
-            try {
-                request.setAttribute("graphContentFile", jsonFileName);
-                request.setAttribute("contentFolder", concreteStateIdentifier);
-                RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/graph.jsp");
-                dispatcher.forward(request, response);
-            } catch (ServletException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        else {
-            response.sendRedirect("/models");
-        }
+			try {
+				request.setAttribute("graphContentFile", jsonFileName);
+				request.setAttribute("contentFolder", concreteStateIdentifier);
+				RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/graph.jsp");
+				dispatcher.forward(request, response);
+			} catch (ServletException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			try {
+				response.sendRedirect("/models");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
-
-
-
-
-
-    }
+	}
 
 }
--- testar/src/org/testar/statemodel/analysis/StateModelServlet.java
+++ testar/src/org/testar/statemodel/analysis/StateModelServlet.java
@@ -1,3 +1,33 @@
+/***************************************************************************************************
+ *
+ * Copyright (c) 2020 - 2023 Universitat Politecnica de Valencia - www.upv.es
+ * Copyright (c) 2020 - 2023 Open Universiteit - www.ou.nl
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************************************/
+
 package org.testar.statemodel.analysis;
 
 import org.testar.statemodel.analysis.representation.AbstractStateModel;
@@ -16,59 +46,65 @@ import java.util.List;
 
 public class StateModelServlet extends HttpServlet {
 
-    protected void doGet(
-            HttpServletRequest request,
-            HttpServletResponse response)
-            throws ServletException, IOException {
+	private static final long serialVersionUID = 275631968009022944L;
 
-        ServletContext servletContext = getServletContext();
-        // todo: this needs proper error handling
-        // fetch a list of the generated models from the analysismanager
-        AnalysisManager analysisManager = (AnalysisManager)servletContext.getAttribute("analysisManager");
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+		ServletContext servletContext = getServletContext();
+		// todo: this needs proper error handling
+		// fetch a list of the generated models from the analysismanager
+		AnalysisManager analysisManager = (AnalysisManager)servletContext.getAttribute("analysisManager");
 
-        try {
-        	List<AbstractStateModel> models = analysisManager.fetchModels();
-        	request.setAttribute("models", models);
-        	RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/models.jsp");
-        	dispatcher.forward(request, response);
-        } catch (ServletException e) {
-        	e.printStackTrace();
-        } catch (IOException e) {
-        	e.printStackTrace();
-        } catch (OCommandExecutionException e) {
-        	// If user tried to analyze an OrientDB database without models, launch an info web page
-        	if(e.getMessage() != null && e.getMessage().contains("Class not found: AbstractStateModel")) {
-        		System.out.println("Database does not contain any model to visualize");
-        		RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/warning.jsp");
-        		dispatcher.forward(request, response);
-        	} else {
-        		e.printStackTrace();
-        	}
-        }
+		try {
+			List<AbstractStateModel> models = analysisManager.fetchModels();
+			request.setAttribute("models", models);
+			RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/models.jsp");
+			dispatcher.forward(request, response);
+		} catch (ServletException se) {
+			se.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} catch (OCommandExecutionException e) {
+			// If user tried to analyze an OrientDB database without models, launch a warning info web page
+			if(e.getMessage() != null && e.getMessage().contains("Class not found: AbstractStateModel")) {
+				forwardToWarningPage(request, response);
+			} else {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    }
+	private void forwardToWarningPage(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("Database does not contain any model to visualize");
+		ServletContext servletContext = request.getServletContext();
+		RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/warning.jsp");
+		try {
+			dispatcher.forward(request, response);
+		} catch (ServletException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    protected void doPost(
-            HttpServletRequest request,
-            HttpServletResponse response)
-            throws ServletException, IOException {
-        ServletContext servletContext = getServletContext();
-        AnalysisManager analysisManager = (AnalysisManager)servletContext.getAttribute("analysisManager");
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+		ServletContext servletContext = getServletContext();
+		AnalysisManager analysisManager = (AnalysisManager)servletContext.getAttribute("analysisManager");
 
-        try {
-            String sequenceId = request.getParameter("sequenceId");
-            List<ActionViz> visualizations = analysisManager.fetchTestSequence(sequenceId);
-            request.setAttribute("contentFolder", sequenceId);
-            request.setAttribute("visualizations", visualizations);
+		try {
+			String sequenceId = request.getParameter("sequenceId");
+			List<ActionViz> visualizations = analysisManager.fetchTestSequence(sequenceId);
+			request.setAttribute("contentFolder", sequenceId);
+			request.setAttribute("visualizations", visualizations);
 
-            RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/sequence.jsp");
-            dispatcher.forward(request, response);
-        } catch (ServletException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
+			RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/sequence.jsp");
+			dispatcher.forward(request, response);
+		} catch (ServletException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
