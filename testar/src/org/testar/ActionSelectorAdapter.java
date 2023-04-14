48adeaa1a5c99bcfb8c9555ac7b9cc3c6a81242e
Olivia Rodriguez Valdes <olivia.rves@gmail.com>
2023-04-14 16:49:11 +0200

    Create ActionSelectorAdapter.java
    create adapter class for action selectors

---
+++ testar/src/org/testar/ActionSelectorAdapter.java
@@ -0,0 +1,38 @@
+package org.testar;
+
+import org.testar.monkey.alayer.Action;
+import org.testar.monkey.alayer.State;
+
+import java.util.Set;
+
+public class ActionSelectorAdapter implements IActionSelector, IActionExecutor, IActionDerive{
+
+    private final Object selector;
+
+    public ActionSelectorAdapter(Object selector) {
+        this.selector = selector;
+    }
+
+    @Override
+    public Set<Action> deriveActions(Set<Action> actions) {
+        if (selector instanceof IActionDerive){
+            return ((IActionDerive) selector).deriveActions(actions);
+        }
+        return actions;
+    }
+
+    @Override
+    public void executeAction(Action action) {
+        if (selector instanceof IActionExecutor){
+            ((IActionExecutor) selector).executeAction(action);
+        }
+    }
+
+    @Override
+    public Action selectAction(State state, Set<Action> actions) {
+        if (selector instanceof IActionSelector){
+            return ((IActionSelector) selector).selectAction(state, actions);
+        }
+        return null;
+    }
+}
