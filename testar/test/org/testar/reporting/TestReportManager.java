2a37e25656743f883a1302a910b6fe94bebf32dc
ferpasri <ferpasri@gmail.com>
2024-10-14 12:23:04 +0200

    Update JUnit tests

--- testar/test/org/testar/reporting/TestReportManager.java
+++ testar/test/org/testar/reporting/TestReportManager.java
@@ -107,7 +107,7 @@ public class TestReportManager {
 		Assert.assertTrue(fileContains("<b>typeActionDescription</b>", htmlReportFile));
 		Assert.assertTrue(fileContains("<b>pasteActionDescription</b>", htmlReportFile));
 		// Verify selected action information
-		Assert.assertTrue(fileContains("<h4>ConcreteID=typeActionConcreteID || typeActionDescription</h4>", htmlReportFile));
+		Assert.assertTrue(fileContains("<h4>ConcreteID=typeActionConcreteID || AbstractID=typeActionAbstractID || typeActionDescription</h4>", htmlReportFile));
 		// Verify verdict information
 		Assert.assertTrue(fileContains("<h2>Test verdict for this sequence: No problem detected.</h2>", htmlReportFile));
 
