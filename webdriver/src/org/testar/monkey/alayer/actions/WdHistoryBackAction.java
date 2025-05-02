4f8e2860c12eba903e610d6fba80512eb95ceeab
Fernando Pastor <32359126+ferpasri@users.noreply.github.com>
2025-05-02 17:13:06 +0200

    Update copys and resources files

--- webdriver/src/org/testar/monkey/alayer/actions/WdHistoryBackAction.java
+++ webdriver/src/org/testar/monkey/alayer/actions/WdHistoryBackAction.java
@@ -1,6 +1,6 @@
 /**
- * Copyright (c) 2018, 2019 Open Universiteit - www.ou.nl
- * Copyright (c) 2019 Universitat Politecnica de Valencia - www.upv.es
+ * Copyright (c) 2018 - 2025 Open Universiteit - www.ou.nl
+ * Copyright (c) 2019 - 2025 Universitat Politecnica de Valencia - www.upv.es
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
--- webdriver/src/org/testar/monkey/alayer/actions/WdRemoteTypeAction.java
+++ webdriver/src/org/testar/monkey/alayer/actions/WdRemoteTypeAction.java
@@ -1,6 +1,6 @@
 /**
- * Copyright (c) 2021 - 2023 Open Universiteit - www.ou.nl
- * Copyright (c) 2021 - 2023 Universitat Politecnica de Valencia - www.upv.es
+ * Copyright (c) 2021 - 2025 Open Universiteit - www.ou.nl
+ * Copyright (c) 2021 - 2025 Universitat Politecnica de Valencia - www.upv.es
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
