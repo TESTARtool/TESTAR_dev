feb7607ea2ebcd6c43fc6eef7a2d737570017b11
ferpasri <ferpasri@gmail.com>
2025-07-04 09:30:51 +0200

    Allow LLM action selector to control HitESC

--- testar/src/org/testar/llm/prompt/ActionStandardPromptGenerator.java
+++ testar/src/org/testar/llm/prompt/ActionStandardPromptGenerator.java
@@ -118,6 +118,10 @@ public class ActionStandardPromptGenerator implements IPromptActionGenerator {
                 case "LeftClickAt":
                     builder.append(String.format("%s: Click on '%s' ", actionId, description));
                     break;
+                case "HitESC":
+                    // TODO: Decide if it makes sense to rely on the LLM to make this control decision
+                    builder.append(String.format("%s: Hit the ESC key", actionId));
+                    break;
                 default:
                     logger.log(Level.WARN, "Unsupported action type for LLM action selection: " + type);
                     break;
