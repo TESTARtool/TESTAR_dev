5edbb257c99c578f75e5aa6d2cf2f4101c77bfa3
ferpasri <ferpasri@gmail.com>
2025-07-15 22:14:48 +0200

    Update testar model

--- testar/resources/oracles/DebugWidgetSelector.java
+++ testar/resources/oracles/DebugWidgetSelector.java
@@ -1,5 +1,6 @@
 import org.testar.monkey.alayer.Verdict;
 import org.testar.monkey.alayer.State;
+import org.testar.monkey.alayer.Tags;
 import org.testar.monkey.alayer.Widget;
 import org.testar.monkey.alayer.webdriver.enums.WdTags;
 import org.testar.oracles.Oracle;
@@ -32,16 +33,21 @@ public class DebugWidgetSelector implements Oracle
                 new SimpleEntry<>("input_text", "age"),
                 new SimpleEntry<>("checkbox", "I accept the terms"),
                 new SimpleEntry<>("radiogroup", "gender"),
+                new SimpleEntry<>("radio", "other"),
                 new SimpleEntry<>("dropdown", "Netherlands"),
                 new SimpleEntry<>("dropdown", "Option C"),
                 new SimpleEntry<>("static_text", "enter your details"),
-                new SimpleEntry<>("label", "Status:"),
+                //new SimpleEntry<>("label", "Status:"),
+                new SimpleEntry<>("static_text", "Status:"), 
                 new SimpleEntry<>("image", "placeholder image"),
                 new SimpleEntry<>("link", "Go to homepage"),
-                new SimpleEntry<>("panel", "Settings"),
+                //new SimpleEntry<>("panel", "Settings"),
+                new SimpleEntry<>("panel", "panel_main"),
+                new SimpleEntry<>("static_text", "Settings"),
                 new SimpleEntry<>("input_text", "Child input"),
                 new SimpleEntry<>("checkbox", "Receive updates"),
-                new SimpleEntry<>("label", "container"),
+                //new SimpleEntry<>("label", "container"),
+                new SimpleEntry<>("static_text", "container"), 
                 new SimpleEntry<>("input_text", "Generic text input")
         );
 
@@ -60,12 +66,12 @@ public class DebugWidgetSelector implements Oracle
         List<Widget> widgets = runWidgetSelector(state, roleString, searchString);
 
         if(widgets.isEmpty())
-            System.out.println("Selector: " + roleString + " & '" +  searchString + "' didn't find any widgets");
+            System.out.println("NOT FOUND Selector: " + roleString + " & '" +  searchString + "' didn't find any widgets");
         else
         {
-            System.out.println("Selector: " + roleString + " & '" + searchString + "'");
+            System.out.println("Found! Selector: " + roleString + " & '" + searchString + "'");
             for (Widget widget : widgets) {
-                System.out.println("Widget: " + widget.toString());
+                System.out.println("Widget: " + widget.get(Tags.Desc, ""));
             }
         }
     }
