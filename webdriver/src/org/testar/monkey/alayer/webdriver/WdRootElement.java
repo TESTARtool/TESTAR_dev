cbdda975936840b5f197d8eece105c3af1615c4f
ferpasri <ferpasri@gmail.com>
2025-09-10 20:38:04 +0200

    Fix potential null in WdRootElement scroll

--- webdriver/src/org/testar/monkey/alayer/webdriver/WdRootElement.java
+++ webdriver/src/org/testar/monkey/alayer/webdriver/WdRootElement.java
@@ -89,8 +89,9 @@ public class WdRootElement extends WdElement {
     vScrollViewSize = 100.0 * clientHeight / scrollHeight;
 
     // TESTAR script function that compares document body height with browser height
-    if (WdDriver.executeScript("return isPageVerticalScrollable()") instanceof Boolean) {
-      vScroll = (Boolean)WdDriver.executeScript("return isPageVerticalScrollable()");
+    Object _isPageVerticalScrollable = WdDriver.executeScript("return isPageVerticalScrollable()");
+    if (_isPageVerticalScrollable instanceof Boolean) {
+      vScroll = (Boolean)_isPageVerticalScrollable;
       vScrollViewSize = 100.0 * rect.height() / clientHeight;
     }
 
@@ -102,8 +103,9 @@ public class WdRootElement extends WdElement {
     hScrollViewSize = 100.0 * clientWidth / scrollWidth;
 
     // TESTAR script function that compares document body width with browser width
-    if (WdDriver.executeScript("return isPageHorizontalScrollable()") instanceof Boolean) {
-      hScroll = (Boolean) WdDriver.executeScript("return isPageHorizontalScrollable()");
+    Object _isPageHorizontalScrollable = WdDriver.executeScript("return isPageHorizontalScrollable()");
+    if (_isPageHorizontalScrollable instanceof Boolean) {
+      hScroll = (Boolean) _isPageHorizontalScrollable;
       hScrollViewSize = 100.0 * rect.width() / clientWidth;
     }
 
