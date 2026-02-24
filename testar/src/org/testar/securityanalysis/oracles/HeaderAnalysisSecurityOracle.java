1b087de9178f383aa64afde455093e3a1597050f
ferpasri <ferpasri@gmail.com>
2026-02-24 11:01:42 +0100

    Update devtools dependencies to v145

--- testar/src/org/testar/securityanalysis/oracles/HeaderAnalysisSecurityOracle.java
+++ testar/src/org/testar/securityanalysis/oracles/HeaderAnalysisSecurityOracle.java
@@ -31,8 +31,8 @@
 package org.testar.securityanalysis.oracles;
 
 import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v144.network.Network;
-import org.openqa.selenium.devtools.v144.network.model.Headers;
+import org.openqa.selenium.devtools.v145.network.Network;
+import org.openqa.selenium.devtools.v145.network.model.Headers;
 import org.testar.monkey.alayer.Verdict;
 import org.testar.monkey.alayer.webdriver.WdDriver;
 import org.testar.securityanalysis.NetworkCollector;
