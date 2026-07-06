8e17b019e177711916750f0f29a146f5b97edf49
ferpasri <ferpasri@gmail.com>
2026-07-06 13:18:03 +0200

    Add digioffice android invariant regex oracles

--- testar/resources/settings/android_digioffice/Protocol_android_digioffice.java
+++ testar/resources/settings/android_digioffice/Protocol_android_digioffice.java
@@ -508,19 +508,23 @@ public class Protocol_android_digioffice extends AndroidProtocol {
         Oracle documentUploadInfoAllowDashOracle = new AndroidDigiOfficeDocumentUploadInfoIsNotEmptyNotNAAllowDash();
         addNewVerdicts(state, verdicts, documentUploadInfoAllowDashOracle.getVerdicts(state));
 
-        // 25) Document list item info is not empty not NA
+        // 25) Document upload list item info is not empty not NA
+        Oracle documentUploadListItemInfoOracle = new AndroidDigiOfficeDocumentUploadListItemInfoIsNotEmptyNotNA();
+        addNewVerdicts(state, verdicts, documentUploadListItemInfoOracle.getVerdicts(state));
+
+        // 26) Document list item info is not empty not NA
         Oracle documentListItemInfoOracle = new AndroidDigiOfficeDocumentListItemInfoIsNotEmptyNotNA();
         addNewVerdicts(state, verdicts, documentListItemInfoOracle.getVerdicts(state));
 
-        // 26) Task list item info is not empty not NA
+        // 27) Task list item info is not empty not NA
         Oracle taskListItemInfoOracle = new AndroidDigiOfficeTaskListItemInfoIsNotEmptyNotNA();
         addNewVerdicts(state, verdicts, taskListItemInfoOracle.getVerdicts(state));
 
-        // 27) Task info row is not empty not NA
+        // 28) Task info row is not empty not NA
         Oracle taskInfoRowOracle = new AndroidDigiOfficeTaskInfoRowIsNotEmptyNotNA();
         addNewVerdicts(state, verdicts, taskInfoRowOracle.getVerdicts(state));
 
-        // 28) Task info row is not empty not NA and allows dash
+        // 29) Task info row is not empty not NA and allows dash
         Oracle taskInfoRowAllowDashOracle = new AndroidDigiOfficeTaskInfoRowIsNotEmptyNotNAAllowDash();
         addNewVerdicts(state, verdicts, taskInfoRowAllowDashOracle.getVerdicts(state));
 
